5a36935f7168e66d57ff686120d50953e0122bf9
LienChan <2434483560@qq.com>
2025-05-08 20:47:23 +0800

    添加静态方法声明，清理Uninit中的无用代码。

--- UIOptimization/NoAutoClosePartyFinder.cs
+++ UIOptimization/NoAutoClosePartyFinder.cs
@@ -13,14 +13,14 @@ public unsafe class NoAutoClosePartyFinder : DailyModuleBase
         Title = GetLoc("NoAutoClosePartyFinderTitle", "防止招募板自动关闭"),
         Description = GetLoc("NoAutoClosePartyFinderDescription", "当小队成员变化时阻止招募板自动关闭。"),
         Category = ModuleCategories.UIOptimization,
-        Author = ["Nyy/YLCHEN"]
+        Author = ["Nyy", "YLCHEN"]
     };
 
     private delegate void LookingForGroupHideDelegate(AgentLookingForGroup* thisPtr);
 
     private static readonly CompSig LookingForGroupHideSig = new("48 89 5C 24 ?? 57 48 83 EC 20 83 A1 ?? ?? ?? ?? ??");
 
-    private Hook<LookingForGroupHideDelegate>? LookingForGroupHideHook;
+    private static Hook<LookingForGroupHideDelegate>? LookingForGroupHideHook;
 
     private static DateTime HookEndsAt;
 
@@ -34,31 +34,22 @@ public unsafe class NoAutoClosePartyFinder : DailyModuleBase
 
     private static void OnPreReceiveMessage(ref bool isPrevented, ref uint logMessageID)
     {
-        if (logMessageID == 947)
-        {
-            isPrevented = true;
-            HookEndsAt = DateTime.UtcNow.AddSeconds(1);
-            return;
-        }
+        if (logMessageID != 947) return;
+
+        isPrevented = true;
+        HookEndsAt = DateTime.UtcNow.AddSeconds(1);
     }
 
-    private void LookingForGroupHideDetour(AgentLookingForGroup* thisPtr)
+    private static void LookingForGroupHideDetour(AgentLookingForGroup* thisPtr)
     {
-        if (DateTime.UtcNow < HookEndsAt)
-        {
-            return;
-        }
+        if (DateTime.UtcNow < HookEndsAt) return;
+
         LookingForGroupHideHook?.Original(thisPtr);
     }
 
     public override void Uninit()
     {
         LogMessageManager.Unregister(OnPreReceiveMessage);
-
-        LookingForGroupHideHook?.Disable();
-        LookingForGroupHideHook?.Dispose();
-        LookingForGroupHideHook = null;
-
         base.Uninit();
     }
 }
