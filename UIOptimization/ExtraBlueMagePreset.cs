f1d5ed6d6299260a72e64211945af5677d57ab64
MisaUo <lzh_99@sjtu.edu.cn>
2025-06-18 15:06:39 +0900

    Fix

--- UIOptimization/ExtraBlueMagePreset.cs
+++ UIOptimization/ExtraBlueMagePreset.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using DailyRoutines.Abstracts;
+using DailyRoutines.Infos;
 using DailyRoutines.Windows;
 using FFXIVClientStructs.FFXIV.Client.Game;
 
@@ -17,9 +18,9 @@ public class BlueMagePresetEntry
 
 public class BlueMagePresetConfig : ModuleConfiguration
 {
-    public List<BlueMagePresetEntry> Presets { get; set; } = new();
+    public List<BlueMagePresetEntry> Presets { get; set; } = [];
     public string NewPresetName = string.Empty;
-    public int? RenameIndex = null;
+    public int? RenameIndex;
 }
 
 public unsafe class ExtraBlueMagePreset : DailyModuleBase
@@ -37,137 +38,121 @@ public unsafe class ExtraBlueMagePreset : DailyModuleBase
 
     public override void Init()
     {
-        Overlay = new Overlay(this);
+        Overlay ??= new Overlay(this);
+        Overlay.Flags |=  ImGuiWindowFlags.AlwaysAutoResize;
         Config = LoadConfig<BlueMagePresetConfig>();
 
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup,   "AOZNotebook", OnAddon);
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "AOZNotebook", OnAddon);
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "AOZNotebook", OnAddon);
-
-        var addon = GetAddonByName("AOZNotebook");
-        if (IsAddonAndNodesReady(addon))
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup,  "AOZNotebook", OnAddon);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,   "AOZNotebook", OnAddon);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize,"AOZNotebook", OnAddon);
+        
+        if (IsAddonAndNodesReady(AOZNotebook))
             OnAddon(AddonEvent.PostSetup, null);
     }
 
     public override void OverlayUI()
     {
-        var addon = GetAddonByName("AOZNotebook");
-        if (addon == null || !addon->IsVisible) return;
+        var addon = AOZNotebook;
+        if (!IsAddonAndNodesReady(addon)) return;
+        
+        var resNode = addon->GetNodeById(5);
+        if (resNode == null) return;
 
-        var posX = Math.Max(addon->X - 320, 20);
-        var posY = Math.Max(addon->Y + 40, 20);
-        ImGui.SetNextWindowPos(new Vector2(posX, posY), ImGuiCond.Always);
-        ImGui.SetNextWindowSizeConstraints(new Vector2(320, 100), new Vector2(320, 600));
-
-        if (ImGui.Begin(GetLoc("ExtraBlueMagePreset"), ImGuiWindowFlags.NoResize | ImGuiWindowFlags.AlwaysAutoResize))
+        var nodeState = NodeState.Get(resNode);
+        var overlayWidth = nodeState.Size.X;
+        var overlayHeight = nodeState.Size.Y;
+        var pos = new Vector2(addon->GetX() - ImGui.GetWindowSize().X - 10 , addon->GetY() + 20);
+        ImGui.SetWindowPos(pos);
+        ImGui.SetWindowSize(new Vector2(overlayWidth, overlayHeight*2));
+        
+        ImGui.TextColored(LightSkyBlue, GetLoc("ExtraBlueMagePreset-BlueMagePreset")); // 青魔法预设
+        ImGui.Separator();
+        using (var presetList = ImRaii.Child("list", new Vector2(overlayWidth - 1, overlayHeight), true))
         {
-            ImGui.TextColored(new Vector4(0.3f, 0.7f, 1.0f, 1.0f), GetLoc("BlueMagePresets")); // 自定义技能预设
-            ImGui.Separator();
+            if (!presetList) return;
 
-            float listMaxHeight = 400f;
-            if (ImGui.BeginChild("##presetList", new Vector2(0, listMaxHeight), true))
+            for (var i = 0; i < Config.Presets.Count; i++)
             {
-                for (int i = 0; i < Config.Presets.Count; i++)
+                var preset = Config.Presets[i];
+                using var id = ImRaii.PushId(i);
+                using var group = ImRaii.Group();
+
+                var nameFieldWidth = ImGui.GetContentRegionAvail().X - 88f;
+
+                ImGui.SetNextItemWidth(50f);
+                if (ImGui.Button(GetLoc("Apply") + $"##{i}"))
+                    ApplyCustomPreset(preset.Actions);
+
+                ImGui.SameLine();
+                
+                ImGui.SetNextItemWidth(nameFieldWidth);
+                if (Config.RenameIndex == i)
                 {
-                    var preset = Config.Presets[i];
-                    ImGui.PushID(i);
-                    ImGui.BeginGroup();
-
-                    float totalWidth = ImGui.GetContentRegionAvail().X;
-                    float applyBtnWidth = 50f;
-                    float deleteBtnWidth = 30f;
-                    float spacing = 4f;
-                    float nameFieldWidth = totalWidth - applyBtnWidth - deleteBtnWidth - spacing * 2;
-
-                    ImGui.PushItemWidth(applyBtnWidth);
-                    if (ImGui.Button(GetLoc("Apply") + $"##{i}")) // 应用
-                        ApplyCustomPreset(preset.Actions);
-                    ImGui.PopItemWidth();
-
-                    ImGui.SameLine();
-                    ImGui.PushItemWidth(nameFieldWidth);
-                    if (Config.RenameIndex == i)
+                    var nameBuffer = preset.Name;
+                    if (ImGui.InputText($"##rename{i}", ref nameBuffer, 64, ImGuiInputTextFlags.EnterReturnsTrue))
                     {
-                        string nameBuffer = preset.Name;
-                        if (ImGui.InputText($"##rename{i}", ref nameBuffer, 64, ImGuiInputTextFlags.EnterReturnsTrue))
-                        {
-                            Config.Presets[i].Name = nameBuffer;
-                            Config.RenameIndex = null;
-                            Config.Save(this);
-                        }
+                        Config.Presets[i].Name = nameBuffer;
+                        Config.RenameIndex = null;
+                        Config.Save(this);
+                        break;
                     }
-                    else
-                    {
-                        if (ImGui.Selectable(preset.Name, false, ImGuiSelectableFlags.AllowDoubleClick, new Vector2(nameFieldWidth, 0)))
-                        {
-                            if (ImGui.IsMouseDoubleClicked(ImGuiMouseButton.Left))
-                                Config.RenameIndex = i;
-                        }
-                    }
-                    ImGui.PopItemWidth();
+                }
+                else
+                {
+                    ImGui.Selectable(preset.Name, false, ImGuiSelectableFlags.AllowDoubleClick, new Vector2(nameFieldWidth, 0));
+                    if (ImGui.IsItemHovered() && ImGui.IsMouseDoubleClicked(ImGuiMouseButton.Left))
+                        Config.RenameIndex = i;
+                }
 
-                    ImGui.SameLine();
-                    ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(2, 2));
-                    ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 4);
-                    if (ImGui.Button($"\uf1f8##{i}", new Vector2(deleteBtnWidth, 28)))
+                ImGui.SameLine();
+                using (ImRaii.PushStyle(ImGuiStyleVar.FramePadding, new Vector2(2, 2)))
+                using (ImRaii.PushStyle(ImGuiStyleVar.FrameRounding, 4))
+                {
+                    if (ImGui.Button($"\uf1f8##{i}", new Vector2(30, 28)))
                     {
                         Config.Presets.RemoveAt(i);
                         Config.Save(this);
-                        NotificationInfo(GetLoc("PresetDeleted") + $":{preset.Name}"); // 已删除预设：
+                        NotificationInfo(GetLoc("ExtraBlueMagePreset-PresetDeleted") + $":{preset.Name}");// 清空全部预设
+                        break;
                     }
-                    ImGui.PopStyleVar(2);
-
-                    ImGui.EndGroup();
-                    ImGui.Spacing();
-                    ImGui.PopID();
                 }
-            }
-            ImGui.EndChild();
 
-            ImGui.InputTextWithHint(GetLoc("NewPresetLabel"), GetLoc("NewPresetPlaceholder"), ref Config.NewPresetName, 64); // 新预设名 / 请输入新预设的名称
-            if (ImGui.Button(GetLoc("SaveCurrentAsNewPreset")) && !string.IsNullOrWhiteSpace(Config.NewPresetName)) // 保存当前为新预设
-            {
-                SaveCurrentPreset(Config.NewPresetName);
-                Config.NewPresetName = string.Empty;
-            }
-
-            if (ImGui.Button(GetLoc("ClearAllPresets"))) // 清空全部预设
-            {
-                Config.Presets.Clear();
-                Config.Save(this);
-                NotificationInfo(GetLoc("AllPresetsCleared")); // 已清空所有预设
+                ImGui.Spacing();
             }
         }
-        ImGui.End();
+        
+        ImGui.SetNextItemWidth(overlayWidth);
+        ImGui.InputTextWithHint("##NewPreset", GetLoc("ExtraBlueMagePreset-NewPresetName"), ref Config.NewPresetName, 64); // 请输入新预设的名称
+        if (ImGui.Button(GetLoc("ExtraBlueMagePreset-SaveCurrentAsNewPreset")) && !string.IsNullOrWhiteSpace(Config.NewPresetName)) // 保存当前为新预设
+        {
+            SaveCurrentPreset(Config.NewPresetName);
+            Config.NewPresetName = string.Empty;
+        }
+
+        if (ImGui.Button(GetLoc("ExtraBlueMagePreset-ClearAllPresets"))) // 清空全部预设
+        {
+            Config.Presets.Clear();
+            Config.Save(this);
+            NotificationInfo(GetLoc("ExtraBlueMagePreset-AllPresetsCleared")); // 已清空所有预设
+        }
     }
 
     private void OnAddon(AddonEvent type, AddonArgs? args)
     {
-        var addon = GetAddonByName("AOZNotebook");
-        if (addon == null) return;
-
         Overlay!.IsOpen = type switch
         {
             AddonEvent.PostSetup   => true,
             AddonEvent.PreFinalize => false,
             _                      => Overlay.IsOpen
         };
-
-        if (type == AddonEvent.PostDraw && Throttler.Throttle("BlueMagePresetOverlayCheck"))
-        {
-            if (addon->AtkValues != null && addon->AtkValues->Int >= 9)
-                Overlay.IsOpen = false;
-            else
-                Overlay.IsOpen = true;
-        }
     }
 
     private void SaveCurrentPreset(string name)
     {
         var actionManager = ActionManager.Instance();
-        uint[] actions = new uint[24];
+        var actions = new uint[24];
 
-        for (int i = 0; i < 24; i++)
+        for (var i = 0; i < 24; i++)
             actions[i] = actionManager->GetActiveBlueMageActionInSlot(i);
 
         Config.Presets.Add(new BlueMagePresetEntry
@@ -177,14 +162,14 @@ public unsafe class ExtraBlueMagePreset : DailyModuleBase
         });
         Config.Save(this);
 
-        NotificationSuccess(GetLoc("PresetSaved") + $":{name}"); // 已保存当前技能配置为预设：
+        NotificationSuccess(GetLoc("ExtraBlueMagePreset-PresetSaved") + $":{name}"); // 已保存当前技能配置为预设：
     }
 
     private void ApplyCustomPreset(uint[] preset)
     {
         if (preset.Length != 24)
         {
-            NotificationError(GetLoc("InvalidPresetData")); // 预设数据不正确
+            NotificationError(GetLoc("ExtraBlueMagePreset-InvalidPresetData")); // 预设数据不正确
             return;
         }
 
@@ -193,13 +178,13 @@ public unsafe class ExtraBlueMagePreset : DailyModuleBase
         Span<uint> current = stackalloc uint[24];
         Span<uint> final   = stackalloc uint[24];
 
-        for (int i = 0; i < 24; i++)
+        for (var i = 0; i < 24; i++)
         {
             current[i] = actionManager->GetActiveBlueMageActionInSlot(i);
             final[i]   = preset[i];
         }
 
-        for (int i = 0; i < 24; i++)
+        for (var i = 0; i < 24; i++)
         {
             if (final[i] == 0) continue;
 
@@ -221,7 +206,7 @@ public unsafe class ExtraBlueMagePreset : DailyModuleBase
                 actionManager->AssignBlueMageActionToSlot(i, final[i]);
         }
 
-        NotificationSuccess(GetLoc("PresetApplied")); // 已应用预设
+        NotificationSuccess(GetLoc("ExtraBlueMagePreset-PresetApplied")); // 已应用预设
     }
 
     public override void Uninit()
