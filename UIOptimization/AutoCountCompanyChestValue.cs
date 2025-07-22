953f9f9fa7a47ed9d09548a2158cc0d7f355253c
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-07-22 15:35:12 +0800

    Update AutoCountCompanyChestValue.cs

--- UIOptimization/AutoCountCompanyChestValue.cs
+++ UIOptimization/AutoCountCompanyChestValue.cs
@@ -1,6 +1,6 @@
-﻿using System.Numerics;
+using System.Globalization;
+using System.Linq;
 using DailyRoutines.Abstracts;
-using DailyRoutines.Windows;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Client.Game;
@@ -13,20 +13,21 @@ public class AutoCountCompanyChestValue : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title = GetLoc("统计部队箱中的沉船首饰"),
-        Description = GetLoc("计算部队箱中单一页面的沉船首饰的数量和价值"),
-        Category = ModuleCategories.UIOptimization,
-        Author = ["采购"]
+        Title       = "自动统计部队储物柜变卖用道具价值",
+        Description = "打开部队储物柜后, 自动统计当前页面下所有变卖用道具的总价值金额",
+        Category    = ModuleCategories.UIOptimization,
+        Author      = ["采购"]
     };
-    //需要统计的六种首饰id
-    private static readonly uint[] ItemIds =  [ 22500, 22501, 22502, 22503, 22504, 22505, 22506, 22507 ];
-    //获取当前部队箱的InventoryType
-    private static unsafe InventoryType GetCurrentFcPage(AtkUnitBase* addon) => 
-        addon == null ? InventoryType.FreeCompanyPage1 : (InventoryType)(20000 + addon->AtkValues[2].UInt);
+
+    private static readonly uint[] ItemIDs = 
+        LuminaGetter.Get<Item>().Where(x => x.ItemSortCategory.Value.Param == 150).Select(x => x.RowId).ToArray();
+
+    private static long LastTotalPrice;
+    
     protected override void Init()
     {
-        TaskHelper ??= new() { TimeLimitMS = 5_000 };
-        Overlay    ??= new Overlay(this);
+        Overlay ??= new(this);
+        
         DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "FreeCompanyChest", CheckFcChestAddon);
         DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "FreeCompanyChest", CheckFcChestAddon);
     }
@@ -45,44 +46,72 @@ public class AutoCountCompanyChestValue : DailyModuleBase
             AddonEvent.PreFinalize => false,
             _                      => Overlay.IsOpen
         };
-        if (type == AddonEvent.PreFinalize)
-            TaskHelper.Abort();
+        
+        LastTotalPrice = 0;
     }
     
-    public override unsafe void OverlayUI()
-    {   
-        if (!TryGetAddonByName("FreeCompanyChest", out var addon)) return;
-        ImGui.SetWindowPos(new Vector2(addon->GetNodeById(108)->ScreenX, addon->GetY() - ImGui.GetWindowSize().Y));
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("部队箱单一页面的沉船首饰统计");
-        if (ImGui.Button("点我"))
-            Check(addon);
-    }
-    
-    // 检查部队箱中的沉船首饰数量和价值
-    private static unsafe void Check(AtkUnitBase* addon)
+    protected override unsafe void OverlayUI()
     {
-        // 检查部队箱的库存界面是否打开
-        if (!addon->GetNodeById(22)->NodeFlags.HasFlag(NodeFlags.Visible))
+        if (FreeCompanyChest == null)
         {
-            Chat("请先点开部队箱的库存界面");
+            Overlay.IsOpen = false;
             return;
-        } 
-        var manager = InventoryManager.Instance();
-        if (manager == null) return;
-        var fcPage = GetCurrentFcPage(addon);
-        var totalPrice = 0;
-        foreach (var item in ItemIds)
+        }
+
+        var node = FreeCompanyChest->GetNodeById(108);
+        if (!IsAddonAndNodesReady(FreeCompanyChest) || node == null)
         {
-            if (LuminaGetter.TryGetRow(item, out Item itemData) == false) continue;
+            Overlay.Flags |= ImGuiWindowFlags.NoBackground;
+            return;
+        }
+        
+        if (Throttler.Throttle("AutoDisplayFCCSubmarineItemsValue-OnUpdate"))
+            LastTotalPrice = TryGetTotalPrice(out var totalPrice) ? totalPrice : 0;
+
+        if (LastTotalPrice == 0)
+        {
+            Overlay.Flags |= ImGuiWindowFlags.NoBackground;
+            return;
+        }
+        
+        Overlay.Flags &= ~ImGuiWindowFlags.NoBackground;
+        
+        ImGui.SetWindowPos(new(node->ScreenX, FreeCompanyChest->GetY() - ImGui.GetWindowSize().Y));
+        
+        ImGui.Text($"变卖用道具总价值: {FormatNumber(LastTotalPrice)}");
+    }
+
+    private static unsafe bool TryGetTotalPrice(out long totalPrice)
+    {
+        totalPrice = 0;
+
+        if (!IsAddonAndNodesReady(FreeCompanyChest)) return false;
+        
+        var manager = InventoryManager.Instance();
+        if (manager == null) return false;
+
+        var fcPage = GetCurrentFcPage(FreeCompanyChest);
+        if (fcPage == InventoryType.Invalid) return false;
+        
+        foreach (var item in ItemIDs)
+        {
+            if (!LuminaGetter.TryGetRow(item, out Item itemData)) continue;
+            
             var itemCount = manager->GetItemCountInContainer(item, fcPage);
             if (itemCount == 0) continue;
+            
             var price = itemData.PriceLow;
-            Chat($"物品{itemData.Name}的数量为:{itemCount} 单个物品价值为:{price} 单个物品总价值为:{itemCount * price} Gil");
-            totalPrice += (int)(itemCount * price);
+            totalPrice += itemCount * price;
         }
-        Chat(totalPrice > 0
-                 ? $"部队箱中沉船首饰的总价值为: {totalPrice} Gil"
-                 : "部队箱中没有沉船首饰");
+        
+        return totalPrice > 0;
     }
+    
+    private static string FormatNumber(long number) =>
+        Lang.CurrentLanguage is not ("ChineseSimplified" or "ChineseTraditional") ? 
+            number.ToString(CultureInfo.InvariantCulture) : 
+            FormatNumberByChineseNotation(number, Lang.CurrentLanguage);
+    
+    private static unsafe InventoryType GetCurrentFcPage(AtkUnitBase* addon) => 
+        addon == null || GetNodeVisible(addon->GetNodeById(106)) ? InventoryType.Invalid : (InventoryType)(20000 + addon->AtkValues[2].UInt);
 }
