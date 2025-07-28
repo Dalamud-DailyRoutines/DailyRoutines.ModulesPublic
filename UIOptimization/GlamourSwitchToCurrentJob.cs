374638518fa456889d77f8e22a6149e1a4a7acdf
ECSS 11 <53446819+Ecss11@users.noreply.github.com>
2025-07-28 17:55:17 -0500

    fix: filter does not apply on switching job

--- UIOptimization/GlamourSwitchToCurrentJob.cs
+++ UIOptimization/GlamourSwitchToCurrentJob.cs
@@ -1,8 +1,6 @@
-﻿using System.Text.RegularExpressions;
-using DailyRoutines.Abstracts;
+﻿using DailyRoutines.Abstracts;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
-using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.UI;
 
 namespace DailyRoutines.ModulesPublic;
@@ -17,29 +15,19 @@ public class GlamourSwitchToCurrentJob : DailyModuleBase
         Author = ["ECSS11"]
     };
 
-    protected override void Init() => DService.AddonLifecycle.RegisterListener(
-        AddonEvent.PostRequestedUpdate, "MiragePrismPrismBox",
-        OnMiragePrismPrismBox);
+    protected override void Init() =>
+        DService.AddonLifecycle.RegisterListener(
+            AddonEvent.PreSetup, "MiragePrismPrismBox",
+            OnMiragePrismPrismBox);
 
-    private unsafe void OnMiragePrismPrismBox(AddonEvent type, AddonArgs args)
+    private static unsafe void OnMiragePrismPrismBox(AddonEvent type, AddonArgs args)
     {
         // 获取当前职业和幻化台 Addon
-        LuminaGetter.TryGetRow<Lumina.Excel.Sheets.ClassJob>(DService.ObjectTable.LocalPlayer.ClassJob.RowId,
-                                                             out var job);
+        var job = DService.ClientState.LocalPlayer.ClassJob;
         var addon = (AddonMiragePrismPrismBox*)args.Addon;
-        var currentJob = job.Name.ToString();
-
-        // 一种基于暴力循环实现的匹配
-        var jobDropdownLength = addon->JobDropdown->List->ListLength;
-        var pattern = new Regex(@"\b" + currentJob + @"\b");
-        for (var i = 1; i < jobDropdownLength; i++)
-        {
-            var currentLabel = addon->JobDropdown->List->GetItemLabel(i).ToString();
-            if (pattern.IsMatch(currentLabel))
-                addon->JobDropdown->SelectItem(i);
-        }
+        addon->Param = (int)job.RowId;
     }
 
     protected override void Uninit() =>
-        DService.AddonLifecycle.UnregisterListener(AddonEvent.PostRequestedUpdate, "MiragePrismPrismBox");
+        DService.AddonLifecycle.UnregisterListener(AddonEvent.PreSetup, "MiragePrismPrismBox");
 }
