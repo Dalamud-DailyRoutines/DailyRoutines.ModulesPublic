88dd84af6387710e029e64a6d8e99bc2e619f15e
joanford <61645196+joanford@users.noreply.github.com>
2025-08-18 15:22:52 +0800

    Create 金蝶砍树.cs

---
+++ UIOperation/金蝶砍树.cs
@@ -0,0 +1,13 @@
+namespace DailyRoutines.ModulesPublic;
+
+public unsafe class 金蝶砍树 : DailyModuleBase
+{
+    protected override void Init()
+    {
+      // TODO
+    }
+
+    protected override void Uninit()
+    {
+    }
+}
