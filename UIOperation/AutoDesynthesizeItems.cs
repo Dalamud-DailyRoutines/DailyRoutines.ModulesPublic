31723ea9b939988b466567d898521954a899e80f
cyf5119 <1005601026@qq.com>
2025-08-20 22:39:51 +0800

    自动分解：改为使用原生UI

--- UIOperation/AutoDesynthesizeItems.cs
+++ UIOperation/AutoDesynthesizeItems.cs
@@ -1,9 +1,10 @@
-using System.Numerics;
 using DailyRoutines.Abstracts;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Memory;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Nodes;
 
 namespace DailyRoutines.ModulesPublic;
 
@@ -15,60 +16,111 @@ public unsafe class AutoDesynthesizeItems : DailyModuleBase
         Description = GetLoc("AutoDesynthesizeItemsDescription"),
         Category    = ModuleCategories.UIOperation,
     };
-    
+
     private static Config ModuleConfig = null!;
+    private static TextNode LableNode;
+    private static CheckboxNode CheckboxNode;
+    private static TextButtonNode StartButtonNode;
+    private static TextButtonNode StopButtonNode;
 
     protected override void Init()
     {
         TaskHelper ??= new() { TimeLimitMS = 10_000 };
-        Overlay    ??= new(this);
 
         ModuleConfig = LoadConfig<Config>() ?? new();
 
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup,   "SalvageDialog",       OnAddon);
         DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup,   "SalvageItemSelector", OnAddonList);
         DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "SalvageItemSelector", OnAddonList);
-        if (IsAddonAndNodesReady(SalvageItemSelector)) 
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup,   "SalvageDialog",       OnAddon);
+        if (IsAddonAndNodesReady(SalvageItemSelector))
             OnAddonList(AddonEvent.PostSetup, null);
-    }
-
-    protected override void OverlayUI()
-    {
-        var addon = SalvageItemSelector;
-        if (addon == null) return;
-
-        var pos = new Vector2(addon->GetX() - ImGui.GetWindowSize().X, addon->GetY() + 6);
-        ImGui.SetWindowPos(pos);
-
-        ImGui.TextColored(LightSkyBlue, GetLoc("AutoDesynthesizeItemsTitle"));
-
-        ImGui.Separator();
-
-        using (ImRaii.Disabled(TaskHelper.IsBusy))
-        {
-            if (ImGui.Checkbox(GetLoc("AutoDesynthesizeItems-SkipHQ"), ref ModuleConfig.SkipWhenHQ))
-                SaveConfig(ModuleConfig);
-
-            if (ImGui.Button(GetLoc("Start"))) 
-                StartDesynthesize();
-        }
-        
-        ImGui.SameLine();
-        if (ImGui.Button(GetLoc("Stop"))) 
-            TaskHelper.Abort();
     }
 
     private void OnAddonList(AddonEvent type, AddonArgs? args)
     {
-        Overlay.IsOpen = type switch
+        switch (type)
         {
-            AddonEvent.PostSetup   => true,
-            AddonEvent.PreFinalize => false,
-            _                      => Overlay.IsOpen,
-        };
-        
-        if (type == AddonEvent.PreFinalize)
-            TaskHelper.Abort();
+            case AddonEvent.PostSetup:
+                if (SalvageItemSelector == null) return;
+
+                if (LableNode == null)
+                {
+                    LableNode = new()
+                    {
+                        IsVisible = true,
+                        Position = new(150, 8),
+                        Size = new(150, 28),
+                        Text = $"{Info.Title}",
+                        FontSize = 14,
+                        AlignmentType = AlignmentType.Left,
+                        TextFlags = TextFlags.AutoAdjustNodeSize | TextFlags.Edge
+                    };
+                    Service.AddonController.AttachNode(LableNode, SalvageItemSelector->RootNode);
+                }
+
+                if (CheckboxNode == null)
+                {
+                    CheckboxNode = new()
+                    {
+                        IsVisible = true,
+                        Position = new(240, 12),
+                        Size = new(75, 20),
+                        IsChecked = ModuleConfig.SkipWhenHQ,
+                        LabelText = GetLoc("AutoDesynthesizeItems-SkipHQ"),
+                        OnClick = newState =>
+                        {
+                            ModuleConfig.SkipWhenHQ = newState;
+                            SaveConfig(ModuleConfig);
+                        }
+                    };
+                    Service.AddonController.AttachNode(CheckboxNode, SalvageItemSelector->RootNode);
+                }
+
+                if (StartButtonNode == null)
+                {
+                    StartButtonNode = new()
+                    {
+                        IsVisible = true,
+                        Position = new(355, 10),
+                        Size = new(100, 28),
+                        Label = GetLoc("Start"),
+                        OnClick = StartDesynthesizeAll
+                    };
+                    Service.AddonController.AttachNode(StartButtonNode, SalvageItemSelector->RootNode);
+                }
+
+                StartButtonNode.IsEnabled = !TaskHelper.IsBusy;
+
+                if (StopButtonNode == null)
+                {
+                    StopButtonNode = new()
+                    {
+                        IsVisible = true,
+                        Position = new(460, 10),
+                        Size = new(100, 28),
+                        Label = GetLoc("Stop"),
+                        OnClick = () => TaskHelper.Abort(),
+                    };
+                    Service.AddonController.AttachNode(StopButtonNode, SalvageItemSelector->RootNode);
+                }
+
+                break;
+            case AddonEvent.PreFinalize:
+                Service.AddonController.DetachNode(LableNode);
+                LableNode = null;
+
+                Service.AddonController.DetachNode(CheckboxNode);
+                CheckboxNode = null;
+
+                Service.AddonController.DetachNode(StartButtonNode);
+                StartButtonNode = null;
+
+                Service.AddonController.DetachNode(StopButtonNode);
+                StopButtonNode = null;
+
+                TaskHelper.Abort();
+                break;
+        }
     }
 
     private static void OnAddon(AddonEvent type, AddonArgs args)
@@ -79,18 +131,25 @@ public unsafe class AutoDesynthesizeItems : DailyModuleBase
         Callback(SalvageDialog, true, 0, 0);
     }
 
+    private void StartDesynthesizeAll()
+    {
+        if (TaskHelper.IsBusy) return;
+
+        TaskHelper.Enqueue(StartDesynthesize, "开始分解全部装备");
+    }
+
     private bool? StartDesynthesize()
     {
         if (OccupiedInEvent) return false;
         if (!IsAddonAndNodesReady(SalvageItemSelector)) return false;
-        
+
         var itemAmount = SalvageItemSelector->AtkValues[9].Int;
         if (itemAmount == 0)
         {
             TaskHelper.Abort();
             return true;
         }
-        
+
         for (var i = 0; i < itemAmount; i++)
         {
             var itemName = MemoryHelper.ReadStringNullTerminated((nint)SalvageItemSelector->AtkValues[(i * 8) + 14].String.Value);
