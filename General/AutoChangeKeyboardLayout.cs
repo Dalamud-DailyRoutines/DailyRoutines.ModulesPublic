bce48b7137f99215a86cd33fe543e143e49cfbfa
JiaXX <573044185@qq.com>
2025-09-19 12:45:55 +0800

    Update

--- General/AutoChangeKeyboardLayout.cs
+++ General/AutoChangeKeyboardLayout.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using DailyRoutines.Abstracts;
-using DailyRoutines.Managers;
-using Dalamud.Plugin.Services;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace DailyRoutines.ModulesPublic;
 
-public class AutoChangeKeyboardLayout : DailyModuleBase
+public partial class AutoChangeKeyboardLayout : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
@@ -18,9 +18,9 @@ public class AutoChangeKeyboardLayout : DailyModuleBase
         Author      = ["JiaXX"]
     };
 
-    protected override void Init() => FrameworkManager.Register(OnUpdate, true, 100);
+    protected override void Init() => DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw, "ChatLog", OnChatLogDraw);
 
-    private static void OnUpdate(IFramework framework)
+    private static void OnChatLogDraw(AddonEvent evt, AddonArgs args)
     {
         if (ChatLogFocusDetector.IsChatLogFocused())
         {
@@ -31,112 +31,112 @@ public class AutoChangeKeyboardLayout : DailyModuleBase
         InputMethodController.SwitchToEnglish();
     }
 
-    protected override void Uninit() => FrameworkManager.Unregister(OnUpdate);
-}
+    protected override void Uninit() => DService.AddonLifecycle.UnregisterListener(OnChatLogDraw);
 
-public static partial class InputMethodController
-{
-    [LibraryImport("user32.dll", EntryPoint = "ActivateKeyboardLayout")]
-    private static partial void ActivateKeyboardLayout(nint hkl, uint Flags);
-
-    [LibraryImport("user32.dll", EntryPoint = "GetKeyboardLayout")]
-    private static partial nint GetKeyboardLayout(uint idThread);
-
-    public static nint currentLayout => GetKeyboardLayout(0);
-
-    public static void SwitchToEnglish()
+    public static partial class InputMethodController
     {
-        try
+        [LibraryImport("user32.dll", EntryPoint = "ActivateKeyboardLayout")]
+        private static partial void ActivateKeyboardLayout(nint hkl, uint Flags);
+
+        [LibraryImport("user32.dll", EntryPoint = "GetKeyboardLayout")]
+        private static partial nint GetKeyboardLayout(uint idThread);
+
+        public static nint currentLayout => GetKeyboardLayout(0);
+
+        public static void SwitchToEnglish()
         {
-            var englishLayout = new nint(0x4090409);
-            if (currentLayout == englishLayout) return;
-            ActivateKeyboardLayout(englishLayout, 0);
+            try
+            {
+                var englishLayout = new nint(0x4090409);
+                if (currentLayout == englishLayout) return;
+                ActivateKeyboardLayout(englishLayout, 0);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
-        catch (Exception e)
+
+        public static void SwitchToChinese()
         {
-            DService.Log.Debug($"切换英文输入法异常: {e.Message}");
+            try
+            {
+                var chineseLayout = new nint(0x8040804);
+                if (currentLayout == chineseLayout) return;
+                ActivateKeyboardLayout(chineseLayout, 0);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
     }
 
-    public static void SwitchToChinese()
+    public static unsafe class ChatLogFocusDetector
     {
-        try
+        public static bool IsChatLogFocused()
         {
-            var chineseLayout = new nint(0x8040804);
-            if (currentLayout == chineseLayout) return;
-            ActivateKeyboardLayout(chineseLayout, 0);
-        }
-        catch (Exception e)
-        {
-            DService.Log.Debug($"切换中文输入法异常: {e.Message}");
-        }
-    }
-}
-
-public static unsafe class ChatLogFocusDetector
-{
-    public static bool IsChatLogFocused()
-    {
-        try
-        {
-            return IsChatLogTextInputFocused() || IsChatLogLikelyFocused();
-        }
-        catch (Exception ex)
-        {
-            DService.Log.Debug($"检测ChatLog焦点时出错: {ex.Message}");
-            return false;
-        }
-    }
-
-    private static bool IsChatLogTextInputFocused()
-    {
-        try
-        {
-            var raptureAtkModule = RaptureAtkModule.Instance();
-            if (raptureAtkModule == null)
+            try
+            {
+                return IsChatLogTextInputFocused() || IsChatLogLikelyFocused();
+            }
+            catch (Exception)
+            {
                 return false;
-
-            var textInputEventInterface = raptureAtkModule->TextInput.TargetTextInputEventInterface;
-            if (textInputEventInterface == null)
-                return false;
-
-            var ownerNode = textInputEventInterface->GetOwnerNode();
-            if (ownerNode == null || ownerNode->GetNodeType() != NodeType.Component)
-                return false;
-
-            var componentNode = (AtkComponentNode*)ownerNode;
-            var componentBase = componentNode->Component;
-            if (componentBase == null || componentBase->GetComponentType() != ComponentType.TextInput)
-                return false;
-
-            var componentTextInput = (AtkComponentTextInput*)componentBase;
-            var addon = componentTextInput->ContainingAddon;
-            if (addon == null)
-                addon = componentTextInput->ContainingAddon2;
-            if (addon == null)
-                addon = RaptureAtkUnitManager.Instance()->GetAddonByNode((AtkResNode*)componentTextInput->OwnerNode);
-            return addon != null && addon->NameString == "ChatLog";
+            }
         }
-        catch (Exception ex)
-        {
-            DService.Log.Debug($"检测ChatLog输入焦点时出错: {ex.Message}");
-            return false;
-        }
-    }
 
-    private static bool IsChatLogLikelyFocused()
-    {
-        try
+        private static bool IsChatLogTextInputFocused()
         {
-            var raptureAtkModule = RaptureAtkModule.Instance();
-            if (raptureAtkModule == null)
+            try
+            {
+                var raptureAtkModule = RaptureAtkModule.Instance();
+                if (raptureAtkModule == null)
+                    return false;
+
+                var textInputEventInterface = raptureAtkModule->TextInput.TargetTextInputEventInterface;
+                if (textInputEventInterface == null)
+                    return false;
+
+                var ownerNode = textInputEventInterface->GetOwnerNode();
+                if (ownerNode == null || ownerNode->GetNodeType() != NodeType.Component)
+                    return false;
+
+                var componentNode = (AtkComponentNode*)ownerNode;
+                var componentBase = componentNode->Component;
+                if (componentBase == null || componentBase->GetComponentType() != ComponentType.TextInput)
+                    return false;
+
+                var componentTextInput = (AtkComponentTextInput*)componentBase;
+                var addon = componentTextInput->ContainingAddon;
+                if (addon == null)
+                    addon = componentTextInput->ContainingAddon2;
+                if (addon == null)
+                {
+                    addon = RaptureAtkUnitManager.Instance()->GetAddonByNode(
+                        (AtkResNode*)componentTextInput->OwnerNode);
+                }
+                return addon != null && addon->NameString == "ChatLog";
+            }
+            catch (Exception)
+            {
                 return false;
-            return raptureAtkModule->TextInput.TargetTextInputEventInterface != null;
+            }
         }
-        catch (Exception ex)
+
+        private static bool IsChatLogLikelyFocused()
         {
-            DService.Log.Debug($"检测ChatLog输入焦点时出错: {ex.Message}");
-            return false;
+            try
+            {
+                var raptureAtkModule = RaptureAtkModule.Instance();
+                if (raptureAtkModule == null)
+                    return false;
+                return raptureAtkModule->TextInput.TargetTextInputEventInterface != null;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
