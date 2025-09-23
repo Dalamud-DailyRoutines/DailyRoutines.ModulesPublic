0038919cbfe957a87bdf997e1d66617013d1ba8f
ShoOtaku <54804635+ShoOtaku@users.noreply.github.com>
2025-09-23 22:13:11 +0800

    Update HideUnwantedBanner.cs

--- UIOptimization/HideUnwantedBanner.cs
+++ UIOptimization/HideUnwantedBanner.cs
@@ -28,41 +28,41 @@ public class HideUnwantedBanner : DailyModuleBase
     };
 
     private static readonly CompSig SetImageTextureSig = new("48 89 5C 24 ?? 57 48 83 EC 30 48 8B D9 89 91");
-    private unsafe delegate void SetImageTextureDelegate(AtkUnitBase* addon, int bannerID, int a3, int soundEffectID);
+    private unsafe delegate void SetImageTextureDelegate(AtkUnitBase* addon, uint bannerID, uint a3, int soundEffectID);
     private static Hook<SetImageTextureDelegate>? SetImageTextureHook;
     private static Config? ModuleConfig;
     
-    private record BannerSetting(int ID, string Label, bool IsCustom = false);
-
-    private static readonly List<BannerSetting> predefinedBanners =
-    [
-        new BannerSetting(120031, GetLoc("HideUnwantedBanner-LevequestAccepted")),
-        new BannerSetting(120032, GetLoc("HideUnwantedBanner-LevequestComplete")),
-        new BannerSetting(120055, GetLoc("HideUnwantedBanner-DeliveryComplete")),
-        new BannerSetting(120081, GetLoc("HideUnwantedBanner-FATEJoined")),
-        new BannerSetting(120082, GetLoc("HideUnwantedBanner-FATEComplete")),
-        new BannerSetting(120083, GetLoc("HideUnwantedBanner-FATEFailed")),
-        new BannerSetting(120084, GetLoc("HideUnwantedBanner-FATEJoinedEXPBonus")),
-        new BannerSetting(120085, GetLoc("HideUnwantedBanner-FATECompleteEXPBonus")),
-        new BannerSetting(120086, GetLoc("HideUnwantedBanner-FATEFailedEXPBonus")),
-        new BannerSetting(120093, GetLoc("HideUnwantedBanner-TreasureObtained")),
-        new BannerSetting(120094, GetLoc("HideUnwantedBanner-TreasureFound")),
-        new BannerSetting(120095, GetLoc("HideUnwantedBanner-VentureCommenced")),
-        new BannerSetting(120096, GetLoc("HideUnwantedBanner-VentureAccomplished")),
-        new BannerSetting(120141, GetLoc("HideUnwantedBanner-VoyageCommenced")),
-        new BannerSetting(120142, GetLoc("HideUnwantedBanner-VoyageComplete")),
-        new BannerSetting(121081, GetLoc("HideUnwantedBanner-TribalQuestAccepted")),
-        new BannerSetting(121082, GetLoc("HideUnwantedBanner-TribalQuestComplete")),
-        new BannerSetting(121561, GetLoc("HideUnwantedBanner-GATEJoined")),
-        new BannerSetting(121562, GetLoc("HideUnwantedBanner-GATEComplete")),
-        new BannerSetting(121563, GetLoc("HideUnwantedBanner-GATEFailed")),
-        new BannerSetting(128370, GetLoc("HideUnwantedBanner-StellarMissionCommenced")),
-        new BannerSetting(128371, GetLoc("HideUnwantedBanner-StellarMissionAbandoned")),
-        new BannerSetting(128372, GetLoc("HideUnwantedBanner-StellarMissionFailed")),
-        new BannerSetting(128373, GetLoc("HideUnwantedBanner-StellarMissionComplete"))
-    ];
-    private static readonly HashSet<int> PredefinedBannerIDs = predefinedBanners.Select(b => b.ID).ToHashSet();
-    private static readonly HashSet<int> SeenBanners = [];
+    private record BannerSetting(int ID, string Label);
+    
+    private static readonly Dictionary<int, string> PredefinedBanners = new()
+    {
+        [120031] = GetLoc("HideUnwantedBanner-LevequestAccepted"),
+        [120032] = GetLoc("HideUnwantedBanner-LevequestComplete"),
+        [120055] = GetLoc("HideUnwantedBanner-DeliveryComplete"),
+        [120081] = GetLoc("HideUnwantedBanner-FATEJoined"),
+        [120082] = GetLoc("HideUnwantedBanner-FATEComplete"),
+        [120083] = GetLoc("HideUnwantedBanner-FATEFailed"),
+        [120084] = GetLoc("HideUnwantedBanner-FATEJoinedEXPBonus"),
+        [120085] = GetLoc("HideUnwantedBanner-FATECompleteEXPBonus"),
+        [120086] = GetLoc("HideUnwantedBanner-FATEFailedEXPBonus"),
+        [120093] = GetLoc("HideUnwantedBanner-TreasureObtained"),
+        [120094] = GetLoc("HideUnwantedBanner-TreasureFound"),
+        [120095] = GetLoc("HideUnwantedBanner-VentureCommenced"),
+        [120096] = GetLoc("HideUnwantedBanner-VentureAccomplished"),
+        [120141] = GetLoc("HideUnwantedBanner-VoyageCommenced"),
+        [120142] = GetLoc("HideUnwantedBanner-VoyageComplete"),
+        [121081] = GetLoc("HideUnwantedBanner-TribalQuestAccepted"),
+        [121082] = GetLoc("HideUnwantedBanner-TribalQuestComplete"),
+        [121561] = GetLoc("HideUnwantedBanner-GATEJoined"),
+        [121562] = GetLoc("HideUnwantedBanner-GATEComplete"),
+        [121563] = GetLoc("HideUnwantedBanner-GATEFailed"),
+        [128370] = GetLoc("HideUnwantedBanner-StellarMissionCommenced"),
+        [128371] = GetLoc("HideUnwantedBanner-StellarMissionAbandoned"),
+        [128372] = GetLoc("HideUnwantedBanner-StellarMissionFailed"),
+        [128373] = GetLoc("HideUnwantedBanner-StellarMissionComplete")
+    };
+    private static readonly Dictionary<int, bool> SeenBanners = [];
+    private static List<BannerSetting> SortedPredefinedBanners = [];
 
     public class Config : ModuleConfiguration
     {
@@ -74,6 +74,11 @@ public class HideUnwantedBanner : DailyModuleBase
         ModuleConfig = LoadConfig<Config>() ?? new Config();
         SetImageTextureHook ??= SetImageTextureSig.GetHook<SetImageTextureDelegate>(OnSetImageTextureDetour);
         SetImageTextureHook.Enable();
+        
+        SortedPredefinedBanners = new List<BannerSetting>();
+        foreach (var kvp in PredefinedBanners)
+            SortedPredefinedBanners.Add(new BannerSetting(kvp.Key, kvp.Value));
+        SortedPredefinedBanners.Sort((b1, b2) => string.Compare(b1.Label, b2.Label, StringComparison.Ordinal));
     }
 
     protected override void ConfigUI()
@@ -85,111 +90,84 @@ public class HideUnwantedBanner : DailyModuleBase
         {
             ImGui.TextWrapped(GetLoc("HideUnwantedBanner-NewlyDetectedBannersHeader"));
             ImGui.Spacing();
-
-            using var seenTable = ImRaii.Table("SeenBannersList", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg);
-            ImGui.TableSetupColumn(GetLoc("Add"), ImGuiTableColumnFlags.WidthFixed);
-            ImGui.TableSetupColumn(GetLoc("Preview"));
-            ImGui.TableHeadersRow();
-            foreach (var bannerID in new List<int>(SeenBanners))
+            
+            using var seenTable = ImRaii.Table("SeenBannersList", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.SizingFixedFit);
+            if (seenTable.Success)
             {
-                ImGui.TableNextRow();
-                ImGui.TableNextColumn();
+                ImGui.TableSetupColumn(GetLoc("Add"), ImGuiTableColumnFlags.WidthFixed, 50 * GlobalFontScale);
+                ImGui.TableSetupColumn(GetLoc("Preview"), ImGuiTableColumnFlags.WidthFixed, 200 * GlobalFontScale);
+                ImGui.TableHeadersRow();
 
-                if (ImGui.Button($"{GetLoc("Add")}##{bannerID}"))
+                foreach (var bannerID in SeenBanners.Keys.ToList())
                 {
-                    ModuleConfig.HiddenBanners.Add(bannerID);
-                    SaveConfig(ModuleConfig);
-                    SeenBanners.Remove(bannerID);
-                }
-                ImGui.TableNextColumn();
-                if (DService.Texture.TryGetFromGameIcon((uint)bannerID, out var icon))
-                {
-                    var wrap = icon.GetWrapOrDefault();
-                    if (wrap != null)
+                    ImGui.TableNextRow();
+                    ImGui.TableNextColumn();
+
+                    if (ImGui.Button($"{GetLoc("Add")}##{bannerID}"))
                     {
-                        var iconHeight = ImGui.GetTextLineHeight() * 4;
-                        var iconWidth = wrap.Width * iconHeight / wrap.Height;
-                        ImGui.Image(wrap.Handle, new Vector2(iconWidth, iconHeight));
+                        ModuleConfig.HiddenBanners.Add(bannerID);
+                        SaveConfig(ModuleConfig);
+                        SeenBanners.Remove(bannerID);
                     }
-                    else
-                    {
-                        ImGui.Text(GetLoc("HideUnwantedBanner-CustomBannerLabel", bannerID));
-                        if (ImGui.IsItemHovered())
-                            ImGui.SetTooltip($"Icon found for {bannerID}, but failed to create texture wrap.");
-                    }
-                }
-                else
-                {
+                    ImGui.TableNextColumn();
                     ImGui.Text(GetLoc("HideUnwantedBanner-CustomBannerLabel", bannerID));
-                    if (ImGui.IsItemHovered()) 
-                        ImGui.SetTooltip($"Could not find icon for ID: {bannerID}");
                 }
             }
+            
             ImGui.Separator();
         }
         ImGui.Spacing();
-        var allBanners = new List<BannerSetting>(predefinedBanners);
-        foreach (var hiddenID in ModuleConfig.HiddenBanners)
+        
+        using var table = ImRaii.Table("BannerList", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.ScrollY | ImGuiTableFlags.SizingFixedFit);
+        if (table.Success)
         {
-            bool isPredefined = false;
-            foreach (var pBanner in predefinedBanners)
+            ImGui.TableSetupColumn(GetLoc("Enable"), ImGuiTableColumnFlags.WidthFixed, 50 * GlobalFontScale);
+            ImGui.TableSetupColumn(GetLoc("Name"), ImGuiTableColumnFlags.WidthFixed, 200 * GlobalFontScale);
+            ImGui.TableHeadersRow();
+
+            foreach (var banner in SortedPredefinedBanners)
+                DrawBannerTableRow(banner);
+
+            foreach (var hiddenID in ModuleConfig.HiddenBanners)
             {
-                if (pBanner.ID == hiddenID)
+                if (!PredefinedBanners.ContainsKey(hiddenID))
                 {
-                    isPredefined = true;
-                    break;
+                    var customLabel = GetLoc("HideUnwantedBanner-CustomBannerLabel", hiddenID);
+                    DrawBannerTableRow(new BannerSetting(hiddenID, customLabel));
                 }
             }
-
-            if (!isPredefined)
-            {
-                var customLabel = GetLoc("HideUnwantedBanner-CustomBannerLabel", hiddenID);
-                allBanners.Add(new BannerSetting(hiddenID, customLabel, true));
-            }
-        }
-        
-        allBanners.Sort((b1, b2) =>
-        {
-            int customCompare = b1.IsCustom.CompareTo(b2.IsCustom);
-            if (customCompare != 0) return customCompare;
-            return string.Compare(b1.Label, b2.Label, StringComparison.Ordinal);
-        });
-        
-        using var table = ImRaii.Table("BannerList", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.ScrollY);
-        ImGui.TableSetupColumn(GetLoc("Enable"), ImGuiTableColumnFlags.WidthFixed);
-        ImGui.TableSetupColumn(GetLoc("Name"));
-        ImGui.TableHeadersRow();
-
-        foreach (var banner in allBanners)
-        {
-            ImGui.TableNextRow();
-            ImGui.TableNextColumn();
-
-            var isHidden = ModuleConfig.HiddenBanners.Contains(banner.ID);
-            if (ImGui.Checkbox($"##{banner.ID}", ref isHidden))
-            {
-                if (isHidden)
-                    ModuleConfig.HiddenBanners.Add(banner.ID);
-                else
-                    ModuleConfig.HiddenBanners.Remove(banner.ID);
-
-                SaveConfig(ModuleConfig);
-            }
-            ImGui.TableNextColumn();
-            ImGui.Text(banner.Label);
         }
     }
     
-    private unsafe void OnSetImageTextureDetour(AtkUnitBase* addon, int bannerID, int a3, int soundEffectID)
+    private void DrawBannerTableRow(BannerSetting banner)
+    {
+        ImGui.TableNextRow();
+        ImGui.TableNextColumn();
+
+        var isHidden = ModuleConfig.HiddenBanners.Contains(banner.ID);
+        if (ImGui.Checkbox($"##{banner.ID}", ref isHidden))
+        {
+            if (isHidden)
+                ModuleConfig.HiddenBanners.Add(banner.ID);
+            else
+                ModuleConfig.HiddenBanners.Remove(banner.ID);
+
+            SaveConfig(ModuleConfig);
+        }
+        ImGui.TableNextColumn();
+        ImGui.Text(banner.Label);
+    }
+    
+    private unsafe void OnSetImageTextureDetour(AtkUnitBase* addon, uint bannerID, uint a3, int soundEffectID)
     {
         var shouldHide = false;
         if (ModuleConfig != null && bannerID > 0)
         {
-            shouldHide = ModuleConfig.HiddenBanners.Contains(bannerID);
-            if (!shouldHide && !PredefinedBannerIDs.Contains(bannerID))
-                SeenBanners.Add(bannerID);
+            shouldHide = ModuleConfig.HiddenBanners.Contains((int)bannerID);
+            if (!shouldHide && !PredefinedBanners.ContainsKey((int)bannerID))
+                SeenBanners.TryAdd((int)bannerID, true);
         }
-        SetImageTextureHook?.Original(addon, shouldHide ? 0 : bannerID, a3, shouldHide ? 0 : soundEffectID);
+        SetImageTextureHook?.Original(addon, shouldHide ? 0 : bannerID, a3, soundEffectID);
     }
 }
 
