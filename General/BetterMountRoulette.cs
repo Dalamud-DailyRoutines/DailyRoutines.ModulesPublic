293c1842f1935bcb9a5e8ed42601c3b31a12a976
ShoOtaku <54804635+ShoOtaku@users.noreply.github.com>
2025-09-25 21:22:32 +0800

    Update BetterMountRoulette.cs
    Update BetterMountRoulette.cs

--- General/BetterMountRoulette.cs
+++ General/BetterMountRoulette.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Collections.Generic;
-using DailyRoutines.Abstracts;
-using DailyRoutines.Infos;
-using Dalamud.Hooking;
-using Dalamud.Bindings.ImGui;
-using FFXIVClientStructs.FFXIV.Client.Game;
-using OmenTools.Helpers;
-using Dalamud.Game;
-using Dalamud.Memory;
-using Dalamud.Interface.Textures.TextureWraps;
+using System.Linq;
 using Dalamud.Interface.Utility.Raii;
+using static OmenTools.Helpers.HelpersOm;
+using static OmenTools.Helpers.ThrottlerHelper;
+using static OmenTools.Helpers.ContentsFinderHelper;
+using static OmenTools.Infos.InfosOm;
+using OmenTools.Infos;
+using OmenTools.Helpers;
+using OmenTools;
+using OmenTools.Service;
+using Dalamud.Bindings.ImGui;
+using DailyRoutines.Abstracts;
+using DailyRoutines.Managers;
+using Dalamud.Interface.Textures.TextureWraps;
+using static DailyRoutines.Infos.Widgets;
+using static DailyRoutines.Infos.Extensions;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Common.Math;
 using Lumina.Excel.Sheets;
-using OmenTools;
-using OmenTools.Infos;
 using TinyPinyin;
-using static DailyRoutines.Infos.Widgets;
 
 namespace DailyRoutines.Modules;
 
@@ -30,25 +34,12 @@ public class BetterMountRoulette : DailyModuleBase
         Author = ["XSZYYS"]
     };
     
-    private static readonly CompSig UseActionSig = new("E8 ?? ?? ?? ?? B0 01 EB B6");
-    private unsafe delegate byte UseActionDelegate(ActionManager* actionManager, uint actionType, uint actionID, long targetedActorID, uint param, uint useType, int pvp, bool* isGroundTarget);
-    private static Hook<UseActionDelegate>? UseActionHook;
     private static Config? ModuleConfig;
-    private static readonly uint[] MountRouletteActionIDs = [9, 24];
-    private static List<Mount>? UnlockedMounts;
-    private static readonly Random random = new();
-    private string normalSearchText = string.Empty;
-    private string pvpSearchText = string.Empty;
-    private List<Mount> SortedNormalMounts = new(); 
-    private List<Mount> SortedPvpMounts = new();
-    private bool normalListDirty = true;
-    private bool pvpListDirty = true;
-    private delegate void MarkDirtyAction();
-    private void MarkNormalListAsDirty() => normalListDirty = true;
-    private void MarkPvpListAsDirty() => pvpListDirty = true;
-    // 拼音缓存
-    private static readonly Dictionary<uint, string> MountPinyinCache = new();
-    
+    private static readonly HashSet<uint> MountRouletteActionIDs = [9, 24];
+    private static readonly Random random = Random.Shared;
+    private static string normalSearchText = string.Empty;
+    private static string pvpSearchText = string.Empty;
+    private static LuminaSearcher<Mount>? MountsSearcher;
     private class Config : ModuleConfiguration
     {
         public HashSet<uint> NormalRouletteMounts = [];
@@ -58,53 +49,50 @@ public class BetterMountRoulette : DailyModuleBase
     protected override unsafe void Init()
     {
         ModuleConfig = LoadConfig<Config>() ?? new Config();
-        UseActionHook ??= UseActionSig.GetHook<UseActionDelegate>(OnUseActionDetour);
-        UseActionHook.Enable();
+        UseActionManager.RegPreUseAction(OnPreUseAction);
         DService.ClientState.Login += OnLogin;
         if (DService.ClientState.IsLoggedIn)
             OnLogin();
     }
     protected override void Uninit()
     {
+        UseActionManager.Unreg(OnPreUseAction);
         DService.ClientState.Login -= OnLogin;
+        SaveConfig(ModuleConfig);
+        MountsSearcher = null;
     }
     
     private unsafe void OnLogin()
     {
+        var allMountsSheet = LuminaGetter.Get<Mount>();
         var playerState = PlayerState.Instance();
-        if (playerState == null)
+        if (allMountsSheet == null || playerState == null) return;
+        var unlockedMounts = new List<Mount>();
+        foreach (var mount in allMountsSheet)
         {
-            DService.Log.Error("无法获取 PlayerState，坐骑列表为空。");
-            UnlockedMounts = new List<Mount>();
-            return;
+            if (IsMountValid(mount, playerState)) 
+                unlockedMounts.Add(mount);
         }
-        var allMountsSheet = DService.Data.GetExcelSheet<Mount>();
-        var tempMountList = new List<Mount>();
-        if (allMountsSheet != null)
-        {
-            foreach (var mount in allMountsSheet)
-            {
-                if (mount.Icon != 0 && !string.IsNullOrEmpty(mount.Singular.ToString()) && playerState->IsMountUnlocked(mount.RowId))
-                    tempMountList.Add(mount);
-            }
-        }
-        tempMountList.Sort((mount1, mount2) => mount1.RowId.CompareTo(mount2.RowId));
-        UnlockedMounts = tempMountList;
-        MountPinyinCache.Clear();
-        foreach (var mount in UnlockedMounts)
-        {
-            var pinyin = PinyinHelper.GetPinyin(mount.Singular.ToString(), string.Empty);
-            MountPinyinCache[mount.RowId] = pinyin.ToLowerInvariant();
-        }
-        // 标记列表需要刷新
-        normalListDirty = true;
-        pvpListDirty = true;
+        
+        MountsSearcher = new LuminaSearcher<Mount>(
+            unlockedMounts,
+            [
+                x => x.Singular.ExtractText()
+            ],
+            x => x.Singular.ExtractText()
+        );
+    }
+    private unsafe bool IsMountValid(Mount mount, PlayerState* playerState)
+    {
+        return playerState->IsMountUnlocked(mount.RowId) &&
+               mount.Icon != 0 &&
+               !string.IsNullOrEmpty(mount.Singular.ExtractText());
     }
     protected override void ConfigUI()
     {
         if (ModuleConfig == null) return;
         
-        if (UnlockedMounts == null)
+        if (MountsSearcher == null)
         {
             ImGui.Text(GetLoc("Settings-NeedLoginToAnyCharacter"));
             return;
@@ -120,70 +108,50 @@ public class BetterMountRoulette : DailyModuleBase
         using var tabBar = ImRaii.TabBar("##MountTabs");
         if (tabBar)
         {
-            using (var tab = ImRaii.TabItem(GetLoc("BetterMountRoulette-NormalAreaTab")))
-            {
-                if (tab)
-                {
-                    ImGui.Text(GetLoc("BetterMountRoulette-NormalMountsHeader"));
-                    if (ImGui.InputTextWithHint("##NormalSearch", GetLoc("Search"), ref normalSearchText, 100))
-                        normalListDirty = true;
-                    if (normalListDirty)
-                    {
-                        UpdateSortedMounts(UnlockedMounts, ModuleConfig.NormalRouletteMounts, normalSearchText,
-                            ref SortedNormalMounts);
-                        normalListDirty = false;
-                    }
-                    using var child = ImRaii.Child("##NormalMountsGrid", new Vector2(-1, 300 * GlobalFontScale), true);
-                    if (child)
-                        DrawMountsGrid(SortedNormalMounts, ModuleConfig.NormalRouletteMounts, MarkNormalListAsDirty);
-                }
-            }
-        }
-        
-        using (var tab = ImRaii.TabItem(GetLoc("BetterMountRoulette-PvpAreaTab")))
-        {
-            if (tab)
-            {
-                ImGui.Text(GetLoc("BetterMountRoulette-PvpMountsHeader"));
-                if (ImGui.InputTextWithHint("##PvpSearch", GetLoc("Search"), ref pvpSearchText, 100))
-                    pvpListDirty = true;
-                if (pvpListDirty)
-                {
-                    UpdateSortedMounts(UnlockedMounts, ModuleConfig.PvpRouletteMounts, pvpSearchText, ref SortedPvpMounts);
-                    pvpListDirty = false;
-                }
-                
-                using var child = ImRaii.Child("##PvpMountsGrid", new Vector2(-1, 300 * GlobalFontScale), true);
-                if (child)
-                    DrawMountsGrid(SortedPvpMounts, ModuleConfig.PvpRouletteMounts, MarkPvpListAsDirty);
-            }
+            DrawTab(GetLoc("BetterMountRoulette-NormalAreaTab"), GetLoc("BetterMountRoulette-NormalMountsHeader"), 
+                    ref normalSearchText, ModuleConfig.NormalRouletteMounts);
+
+            DrawTab(GetLoc("BetterMountRoulette-PvpAreaTab"), GetLoc("BetterMountRoulette-PvpMountsHeader"),
+                    ref pvpSearchText, ModuleConfig.PvpRouletteMounts);
         }
     }
-    
-    private void UpdateSortedMounts(List<Mount> allMounts, HashSet<uint> selectedMounts, string searchText, ref List<Mount> sortedList)
+    private void DrawTab(string tabLabel, string header, ref string searchText, HashSet<uint> selectedMounts)
     {
-        List<Mount> filteredMounts;
-        // 根据搜索文本过滤坐骑
-        if (string.IsNullOrEmpty(searchText))
-            filteredMounts = allMounts;
-        else
+        using var tab = ImRaii.TabItem(tabLabel);
+        if (!tab) return;
+        
+        ImGui.Text(header);
+        ImGui.InputTextWithHint($"##Search{tabLabel}", GetLoc("Search"), ref searchText, 100);
+        
+        List<Mount> mountsToDraw;
+        if (MountsSearcher != null)
         {
-            filteredMounts = new List<Mount>();
-            var lowerSearchText = searchText.ToLowerInvariant();
-            foreach (var mount in allMounts)
+            try
             {
-                // 支持名称和拼音搜索
-                if (mount.Singular.ToString().Contains(searchText, StringComparison.OrdinalIgnoreCase) || (MountPinyinCache.TryGetValue(mount.RowId, out var pinyin) && pinyin.Contains(lowerSearchText)))
-                    filteredMounts.Add(mount);
+                MountsSearcher.Search(searchText);
+                mountsToDraw = SortMountsForDisplay(MountsSearcher.SearchResult, selectedMounts);
+            }
+            catch (Exception ex)
+            {
+                DService.Log.Error(ex, "Error during mount search.");
+                mountsToDraw = [];
             }
         }
+        else
+            mountsToDraw = [];
+        
+        using var child = ImRaii.Child($"##MountsGrid{tabLabel}", new Vector2(-1, 300 * GlobalFontScale), true);
+        if (child)
+            DrawMountsGrid(mountsToDraw, selectedMounts);
+    }
+    private List<Mount> SortMountsForDisplay(List<Mount> mounts, HashSet<uint> selectedMounts)
+    {
+        if (mounts.Count == 0) return [];
 
-        sortedList.Clear();
         var selectedFilteredMounts = new List<Mount>();
         var unselectedFilteredMounts = new List<Mount>();
-
-        // 将过滤后的坐骑分为已选择和未选择两组
-        foreach (var mount in filteredMounts)
+        
+        foreach (var mount in mounts)
         {
             if (selectedMounts.Contains(mount.RowId))
                 selectedFilteredMounts.Add(mount);
@@ -191,11 +159,15 @@ public class BetterMountRoulette : DailyModuleBase
                 unselectedFilteredMounts.Add(mount);
         }
 
-        // 将已选择的坐骑放在前面
-        sortedList.AddRange(selectedFilteredMounts);
-        sortedList.AddRange(unselectedFilteredMounts);
+        selectedFilteredMounts.Sort((m1, m2) => m1.RowId.CompareTo(m2.RowId));
+        unselectedFilteredMounts.Sort((m1, m2) => m1.RowId.CompareTo(m2.RowId));
+        
+        var resultList = new List<Mount>();
+        resultList.AddRange(selectedFilteredMounts);
+        resultList.AddRange(unselectedFilteredMounts);
+        return resultList;
     }
-    private void DrawMountsGrid(List<Mount> mountsToDraw, HashSet<uint> selectedMounts, MarkDirtyAction markAsDirty)
+    private void DrawMountsGrid(List<Mount> mountsToDraw, HashSet<uint> selectedMounts)
     {
         if (mountsToDraw.Count == 0) return;
         var itemWidthEstimate = 120 * GlobalFontScale;
@@ -208,10 +180,9 @@ public class BetterMountRoulette : DailyModuleBase
             foreach (var mount in mountsToDraw)
             {
                 ImGui.TableNextColumn();
-                ImGui.PushID(mount.RowId.ToString());
                 var iconSize = 35 * GlobalFontScale;
                 var isSelected = selectedMounts.Contains(mount.RowId);
-                if (ImGui.Checkbox(string.Empty, ref isSelected))
+                if (ImGui.Checkbox($"##{mount.RowId}", ref isSelected))
                 {
                     if (isSelected)
                         selectedMounts.Add(mount.RowId);
@@ -219,77 +190,49 @@ public class BetterMountRoulette : DailyModuleBase
                         selectedMounts.Remove(mount.RowId);
                     if (ModuleConfig != null)
                         SaveConfig(ModuleConfig);
-                    // 标记列表需要刷新
-                    markAsDirty();
                 }
                 ImGui.SameLine();
-                IDalamudTextureWrap? icon = null;
-                try
-                {
-                    icon = DService.Texture.GetFromGameIcon((uint)mount.Icon).GetWrapOrDefault();
-                }
-                catch
-                {
-                    // ignored
-                }
-                if (icon != null)
-                    ImGui.Image(icon.Handle, new Vector2(iconSize));
+                if (DService.Texture.TryGetFromGameIcon((uint)mount.Icon, out var icon))
+                    ImGui.Image(icon.GetWrapOrEmpty().Handle, new Vector2(iconSize));
                 else
                     ImGui.Dummy(new Vector2(iconSize));
 
                 ImGui.SameLine();
 
+                var mountName = mount.Singular.ExtractText();
                 var textPos = ImGui.GetCursorPos();
-                ImGui.SetCursorPosY(textPos.Y + (iconSize - ImGui.CalcTextSize(mount.Singular.ToString()).Y) / 2f);
-                ImGui.Text(mount.Singular.ToString());
-                ImGui.SetCursorPosY(textPos.Y);
+                ImGui.SetCursorPosY(textPos.Y + (iconSize - ImGui.CalcTextSize(mountName).Y) / 2f);
+                ImGui.Text(mountName);
 
-                ImGui.PopID();
             }
         }
     }
     
-    private unsafe byte OnUseActionDetour(ActionManager* actionManager, uint actionType, uint actionID, long targetedActorID, uint param, uint useType, int pvp, bool* isGroundTarget)
+    private static unsafe void OnPreUseAction(ref bool isPrevented, ref ActionType actionType, ref uint actionID, ref ulong targetID, ref uint extraParam, ref ActionManager.UseActionMode queueState, ref uint comboRouteID)
     {
-        var isRouletteAction = false;
-        foreach (var id in MountRouletteActionIDs)
-        {
-            if (id == actionID)
-            {
-                isRouletteAction = true;
-                break;
-            }
-        }
+        if (actionType is not ActionType.GeneralAction || !MountRouletteActionIDs.Contains(actionID))
+            return;
 
-        if (actionType == 5 && isRouletteAction)
+        if (ModuleConfig == null) return;
+        
+        try
         {
-            try
-            {
-                var isInPvp = DService.ClientState.IsPvP;
-                if (ModuleConfig != null)
-                {
-                    var mountList = isInPvp ? ModuleConfig.PvpRouletteMounts : ModuleConfig.NormalRouletteMounts;
+            var isInPvp = DService.ClientState.IsPvP;
+            var mountList = isInPvp ? ModuleConfig.PvpRouletteMounts : ModuleConfig.NormalRouletteMounts;
 
-                    if (mountList.Count > 0)
-                    {
-                        var mountsAsList = new List<uint>(mountList);
-                        var randomMountID = mountsAsList[random.Next(mountsAsList.Count)];
-                        actionManager->UseAction(ActionType.Mount, randomMountID);
-                        return 0;
-                    }
-                    else
-                    {
-                        OmenTools.Helpers.HelpersOm.NotificationInfo(GetLoc("BetterMountRoulette-ListEmptyError"));
-                        return 0;
-                    }
-                }
-            }
-            catch (Exception ex)
+            if (mountList.Count > 0)
             {
-                DService.Log.Error(ex, "执行坐骑随机时出错");
+                var randomMountID = mountList.ElementAt(random.Next(mountList.Count));
+                ActionManager.Instance()->UseAction(ActionType.Mount, randomMountID);
             }
+            else
+                NotificationInfo(GetLoc("BetterMountRoulette-ListEmptyError"));
         }
-        return UseActionHook.Original(actionManager, actionType, actionID, targetedActorID, param, useType, pvp,
-            isGroundTarget);
+        catch (Exception ex)
+        {
+            DService.Log.Error(ex, "执行随机坐骑时出错");
+        }
+        
+        isPrevented = true;
     }
 }
