3fd6bcb14c81af71c2930b851bb81fc1f29539f5
AtmoOmen <atmoomen2004@outlook.com>
2026-02-14 13:22:56 +0800

    CancelCountdownCommand

--- Assist/CancelCountdownCommand.cs
+++ Assist/CancelCountdownCommand.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using DailyRoutines.Abstracts;
 using DailyRoutines.Managers;
-using Dalamud.Game.Command;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 namespace DailyRoutines.ModulesPublic;
@@ -10,27 +9,32 @@ public class CancelCountdownCommand : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title = GetLoc("CancelCountdownCommandTitle"),
+        Title       = GetLoc("CancelCountdownCommandTitle"),
         Description = GetLoc("CancelCountdownCommandDescription", COMMAND),
-        Category = ModuleCategories.Assist,
-        Author = ["decorwdyun"]
+        Category    = ModuleCategories.Assist,
+        Author      = ["decorwdyun"]
     };
-    
+
+    public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
     private const string COMMAND = "ccd";
 
     private static readonly Action CancelCountdown =
         new CompSig("E8 ?? ?? ?? ?? 45 33 E4 41 C6 47 ?? ?? 45 89 66 30").GetDelegate<Action>();
 
     protected override void Init() =>
-        CommandManager.AddSubCommand(
-            COMMAND, new CommandInfo(OnCommand) { HelpMessage = GetLoc("CancelCountdownCommand-CommandHelp") });
+        CommandManager.AddSubCommand
+        (
+            COMMAND,
+            new(OnCommand) { HelpMessage = GetLoc("CancelCountdownCommand-CommandHelp") }
+        );
 
     public static unsafe void OnCommand(string command, string arguments)
     {
         if (!AgentCountDownSettingDialog.Instance()->Active) return;
         CancelCountdown();
     }
-    
+
     protected override void Uninit() =>
-        CommandManager.RemoveCommand(COMMAND);
+        CommandManager.RemoveSubCommand(COMMAND);
 }
