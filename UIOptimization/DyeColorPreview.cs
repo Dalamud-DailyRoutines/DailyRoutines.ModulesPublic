77a0b737ab296097c8afbc155e0507a3669a00be
Keyin Lin <erx878658607@gmail.com>
2026-05-07 22:38:57 +1000

    Address review comments for DyeColorPreview

--- UIOptimization/DyeColorPreview.cs
+++ UIOptimization/DyeColorPreview.cs
@@ -1,10 +1,8 @@
+using System.Collections.Frozen;
 using System.Numerics;
 using DailyRoutines.Common.Module.Abstractions;
 using DailyRoutines.Common.Module.Enums;
 using DailyRoutines.Common.Module.Models;
-using Dalamud.Game.Text.SeStringHandling;
-using Dalamud.Game.Text.SeStringHandling.Payloads;
-using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Enums;
 using KamiToolKit.Nodes;
@@ -13,134 +11,85 @@ using KamiToolKit.Premade.Node.Simple;
 using Lumina.Excel.Sheets;
 using OmenTools.Interop.Game.Lumina;
 using OmenTools.OmenService;
-using Dalamud.Bindings.ImGui;
 
 namespace DailyRoutines.ModulesPublic;
 
 public unsafe class DyeColorPreview : ModuleBase
 {
-    #region Module
+    #region 模块
 
     public override ModuleInfo Info { get; } = new()
     {
-        Title = Lang.Get("DyeColorPreviewTitle"),
+        Title       = Lang.Get("DyeColorPreviewTitle"),
         Description = Lang.Get("DyeColorPreviewDescription"),
-        Category = ModuleCategory.UIOptimization,
-        Author = ["ErxCharlotte"],
+        Category    = ModuleCategory.UIOptimization,
+        Author      = ["ErxCharlotte"]
     };
 
     public override ModulePermission Permission { get; } = new()
     {
-        AllDefaultEnabled = true,
+        AllDefaultEnabled = true
     };
 
     #endregion
 
-    #region Fields
+    #region 字段
 
-    private DyeInfo? currentDye;
+    private DyeInfo?           currentDye;
     private OverlayController? overlayController;
-    private TooltipModification? tooltipModification;
-    private AtkUnitBase* currentTooltipAddon;
 
     #endregion
 
-    #region Module Lifecycle
+    #region 生命周期
 
     protected override void Init()
     {
-        GameTooltipManager.Instance().RegGenerateItemTooltipModifier(OnItemTooltipGenerate);
+        GameTooltipManager.Instance().RegItemTooltip(OnItemTooltipGenerate);
 
-        overlayController = new OverlayController();
+        overlayController = new();
         overlayController.AddNode(new DyePreviewOverlayNode(() => currentDye, IsCurrentTooltipVisible));
     }
 
     protected override void Uninit()
     {
-        GameTooltipManager.Instance().Unreg(generateItemModifiers: OnItemTooltipGenerate);
-        RemoveTooltipModification();
+        GameTooltipManager.Instance().Unreg(OnItemTooltipGenerate);
 
         overlayController?.Dispose();
         overlayController = null;
         currentDye = null;
-        currentTooltipAddon = null;
     }
 
     #endregion
 
-    #region Tooltip
+    #region 悬浮提示
 
-    private void OnItemTooltipGenerate(AtkUnitBase* addonItemDetail, NumberArrayData* numberArrayData, StringArrayData* stringArrayData)
+    private void OnItemTooltipGenerate(ItemTooltipContext context)
     {
-        RemoveTooltipModification();
         currentDye = null;
-        currentTooltipAddon = null;
 
-        var itemID = AgentItemDetail.Instance()->ItemId % 100_0000;
+        var itemID = context.ItemID % 100_0000;
         if (!Dyes.TryGetValue(itemID, out var dye))
-        {
             return;
-        }
 
-        currentTooltipAddon = addonItemDetail;
         currentDye = dye;
-
-        tooltipModification = GameTooltipManager.Instance().AddItemDetail
-        (
-            itemID,
-            TooltipItemType.ItemDescription,
-            BuildTooltipText(dye),
-            TooltipModifyMode.Append
-        );
     }
-    
-    private void RemoveTooltipModification()
-    {
-        if (tooltipModification == null) return;
 
-        GameTooltipManager.Instance().RemoveItemDetail(tooltipModification);
-        tooltipModification = null;
-    }
-    
     private bool IsCurrentTooltipVisible()
     {
-        if (currentDye == null || currentTooltipAddon == null)
+        if (currentDye == null || ItemDetail == null || !ItemDetail->IsVisible)
             return false;
-        
-        if (currentTooltipAddon->IsVisible)
-            return true;
 
-        currentDye = null;
-        currentTooltipAddon = null;
-        RemoveTooltipModification();
-        return false;
-    }
-
-    private static SeString BuildTooltipText(DyeInfo dye)
-    {
-        return new SeString
-        (
-            new NewLinePayload(),
-            new UIForegroundPayload(3),
-            new TextPayload(Lang.Get("DyeColorPreviewTooltip", dye.StainIDs.Length)),
-            new UIForegroundPayload(0)
-        );
+        return Dyes.ContainsKey((uint)(DService.Instance().GameGUI.HoveredItem % 100_0000));
     }
 
     #endregion
 
-    #region Overlay
+    #region 覆盖层
 
     private sealed class DyePreviewOverlayNode : OverlayNode
     {
         public override OverlayLayer OverlayLayer => OverlayLayer.Foreground;
         public override bool HideWithNativeUi => false;
-
-        private const float BackgroundPadding = 24f;
-        private const float ContentPaddingX = 18f;
-        private const float TitleY = 12f;
-        private const float CountY = 46f;
-        private const float FirstRowY = 84f;
 
         private readonly Func<DyeInfo?> getCurrentDye;
         private readonly Func<bool> isTooltipVisible;
@@ -148,48 +97,48 @@ public unsafe class DyeColorPreview : ModuleBase
 
         private readonly SimpleNineGridNode backgroundNode = new()
         {
-            TexturePath = "ui/uld/EnemyList_hr1.tex",
+            TexturePath        = "ui/uld/EnemyList_hr1.tex",
             TextureCoordinates = new Vector2(96, 80),
-            TextureSize = new Vector2(24, 20),
-            Offsets = new Vector4(8, 8, 8, 8),
-            MultiplyColor = new Vector3(0f),
-            Alpha = 1f,
-            IsVisible = true,
+            TextureSize        = new Vector2(24, 20),
+            Offsets            = new Vector4(8, 8, 8, 8),
+            MultiplyColor      = new Vector3(0f),
+            Alpha              = 1f,
+            IsVisible          = true,
         };
 
         private readonly TextNode titleNode = new()
         {
-            Position = new Vector2(ContentPaddingX, TitleY),
-            Size = new Vector2(560, 34),
-            FontSize = 22,
-            TextFlags = TextFlags.Edge,
-            TextColor = Vector4.One,
+            Position         = new Vector2(CONTENT_PADDING_X, TITLE_Y),
+            Size             = new Vector2(560, 34),
+            FontSize         = 22,
+            TextFlags        = TextFlags.Edge,
+            TextColor        = Vector4.One,
             TextOutlineColor = new Vector4(0, 0, 0, 1),
-            AlignmentType = AlignmentType.TopLeft,
+            AlignmentType    = AlignmentType.TopLeft,
         };
 
         private readonly TextNode countNode = new()
         {
-            Position = new Vector2(ContentPaddingX, CountY),
-            Size = new Vector2(560, 26),
-            FontSize = 16,
-            TextFlags = TextFlags.Edge,
-            TextColor = Vector4.One,
+            Position         = new Vector2(CONTENT_PADDING_X, COUNT_Y),
+            Size             = new Vector2(560, 26),
+            FontSize         = 16,
+            TextFlags        = TextFlags.Edge,
+            TextColor        = Vector4.One,
             TextOutlineColor = new Vector4(0, 0, 0, 1),
-            AlignmentType = AlignmentType.TopLeft,
+            AlignmentType    = AlignmentType.TopLeft,
         };
 
         private DyeInfo? lastDye;
 
         public DyePreviewOverlayNode(Func<DyeInfo?> getCurrentDye, Func<bool> isTooltipVisible)
         {
-            this.getCurrentDye = getCurrentDye;
-            this.isTooltipVisible = isTooltipVisible;
+            this.getCurrentDye     = getCurrentDye;
+            this.isTooltipVisible  = isTooltipVisible;
 
             Size = new Vector2(600, 160);
 
-            backgroundNode.Position = new Vector2(-BackgroundPadding, -BackgroundPadding);
-            backgroundNode.Size = Size + new Vector2(BackgroundPadding * 2f);
+            backgroundNode.Position = new Vector2(-BACKGROUND_PADDING, -BACKGROUND_PADDING);
+            backgroundNode.Size     = Size + new Vector2(BACKGROUND_PADDING * 2f);
             backgroundNode.AttachNode(this);
 
             titleNode.AttachNode(this);
@@ -218,8 +167,8 @@ public unsafe class DyeColorPreview : ModuleBase
 
         private Vector2 GetOverlayPosition()
         {
-            var mouse = ImGui.GetMousePos();
-            var display = ImGui.GetIO().DisplaySize;
+            var mouse    = ImGui.GetMousePos();
+            var display  = ImGui.GetIO().DisplaySize;
             var position = mouse + new Vector2(36f, 24f);
 
             if (position.X + Size.X > display.X)
@@ -233,59 +182,60 @@ public unsafe class DyeColorPreview : ModuleBase
 
         private void Rebuild(DyeInfo dye)
         {
-            titleNode.String = $"★ {Lang.Get(dye.NameKey)}";
+            titleNode.String    = $"★ {Lang.Get(dye.NameKey)}";
             titleNode.TextColor = dye.HighlightColor;
-            countNode.String = Lang.Get("DyeColorPreviewAvailableColors", dye.StainIDs.Length);
+            countNode.String    = Lang.Get("DyeColorPreviewAvailableColors", dye.StainIDs.Length);
 
             ClearColorNodes();
 
-            var count = dye.StainIDs.Length;
-            var compact = count > 40;
-            var perRow = compact ? 5 : 4;
-            var cellWidth = compact ? 112f : 132f;
-            var cellHeight = compact ? 36f : 40f;
-            var squareSize = compact ? 24 : 28;
-            var nameSize = compact ? 14 : 16;
+            var count      = dye.StainIDs.Length;
+            var compact    = count > COMPACT_THRESHOLD;
+            var perRow     = compact ? COMPACT_PER_ROW : NORMAL_PER_ROW;
+            var cellWidth  = compact ? COMPACT_CELL_WIDTH : NORMAL_CELL_WIDTH;
+            var cellHeight = compact ? COMPACT_CELL_HEIGHT : NORMAL_CELL_HEIGHT;
+            var squareSize = compact ? COMPACT_SQUARE_SIZE : NORMAL_SQUARE_SIZE;
+            var nameSize   = compact ? COMPACT_NAME_SIZE : NORMAL_NAME_SIZE;
 
             for (var i = 0; i < count; i++)
                 AddStainNode(dye.StainIDs[i], i, perRow, cellWidth, cellHeight, squareSize, nameSize);
 
             var rows = (int)MathF.Ceiling(count / (float)perRow);
-            Size = new Vector2(perRow * cellWidth + ContentPaddingX * 2f, 104f + rows * cellHeight);
+            Size = new Vector2(perRow * cellWidth + CONTENT_PADDING_X * 2f, BASE_HEIGHT + rows * cellHeight);
 
-            backgroundNode.Position = new Vector2(-BackgroundPadding, -BackgroundPadding);
-            backgroundNode.Size = Size + new Vector2(BackgroundPadding * 2f);
+            backgroundNode.Position = new Vector2(-BACKGROUND_PADDING, -BACKGROUND_PADDING);
+            backgroundNode.Size     = Size + new Vector2(BACKGROUND_PADDING * 2f);
         }
 
         private void AddStainNode(uint stainID, int index, int perRow, float cellWidth, float cellHeight, int squareSize, int nameSize)
         {
-            if (!LuminaGetter.TryGetRow<Stain>(stainID, out var stain)) return;
+            if (!LuminaGetter.TryGetRow<Stain>(stainID, out var stain))
+                return;
 
-            var x = ContentPaddingX + index % perRow * cellWidth;
-            var y = FirstRowY + index / perRow * cellHeight;
+            var x     = CONTENT_PADDING_X + index % perRow * cellWidth;
+            var y     = FIRST_ROW_Y + index / perRow * cellHeight;
             var color = stain.Color.ToVector4();
             color = new Vector4(color.Z, color.Y, color.X, 1f);
 
             AddColorNode(new TextNode
             {
-                Position = new Vector2(x, y),
-                Size = new Vector2(squareSize + 6, squareSize + 6),
-                FontSize = (byte)squareSize,
-                TextColor = color,
+                Position      = new Vector2(x, y),
+                Size          = new Vector2(squareSize + 6, squareSize + 6),
+                FontSize      = (byte)squareSize,
+                TextColor     = color,
                 AlignmentType = AlignmentType.TopLeft,
-                String = "■",
+                String        = "■",
             });
 
             AddColorNode(new TextNode
             {
-                Position = new Vector2(x + squareSize + 8, y + 5),
-                Size = new Vector2(cellWidth - squareSize - 10, cellHeight),
-                FontSize = (byte)nameSize,
-                TextFlags = TextFlags.Edge,
-                TextColor = Vector4.One,
+                Position         = new Vector2(x + squareSize + 8, y + 5),
+                Size             = new Vector2(cellWidth - squareSize - 10, cellHeight),
+                FontSize         = (byte)nameSize,
+                TextFlags        = TextFlags.Edge,
+                TextColor        = Vector4.One,
                 TextOutlineColor = new Vector4(0, 0, 0, 1),
-                AlignmentType = AlignmentType.TopLeft,
-                String = stain.Name.ExtractText(),
+                AlignmentType    = AlignmentType.TopLeft,
+                String           = stain.Name.ExtractText(),
             });
         }
 
@@ -306,17 +256,17 @@ public unsafe class DyeColorPreview : ModuleBase
 
     #endregion
 
-    #region Data
+    #region 数据
 
     private sealed record DyeInfo(string NameKey, Vector4 HighlightColor, uint[] StainIDs);
 
-    private static readonly Dictionary<uint, DyeInfo> Dyes = new()
+    private static readonly FrozenDictionary<uint, DyeInfo> Dyes = new Dictionary<uint, DyeInfo>
     {
         [52254] = new
         (
             "DyeColorPreviewGeneralDye",
             new Vector4(1f, 0.95f, 0.65f, 1f),
-            Enumerable.Range(1, 85).Select(x => (uint)x).ToArray()
+            Enumerable.Range(1, 85).Select(static x => (uint)x).ToArray()
         ),
         [52255] = new
         (
@@ -329,8 +279,32 @@ public unsafe class DyeColorPreview : ModuleBase
             "DyeColorPreviewExtraDye2",
             new Vector4(0.25f, 0.45f, 1f, 1f),
             [95, 96, 97, 98, 99, 100, 121, 122, 123, 124, 125]
-        ),
-    };
+        )
+    }.ToFrozenDictionary();
+
+    #endregion
+
+    #region 常量
+
+    private const float BACKGROUND_PADDING = 24f;
+    private const float CONTENT_PADDING_X  = 18f;
+    private const float TITLE_Y            = 12f;
+    private const float COUNT_Y            = 46f;
+    private const float FIRST_ROW_Y        = 84f;
+    private const float BASE_HEIGHT        = 104f;
+
+    private const int COMPACT_THRESHOLD   = 40;
+    private const int NORMAL_PER_ROW      = 4;
+    private const int COMPACT_PER_ROW     = 5;
+    private const int NORMAL_SQUARE_SIZE  = 28;
+    private const int COMPACT_SQUARE_SIZE = 24;
+    private const int NORMAL_NAME_SIZE    = 16;
+    private const int COMPACT_NAME_SIZE   = 14;
+
+    private const float NORMAL_CELL_WIDTH    = 132f;
+    private const float COMPACT_CELL_WIDTH   = 112f;
+    private const float NORMAL_CELL_HEIGHT   = 40f;
+    private const float COMPACT_CELL_HEIGHT  = 36f;
 
     #endregion
 }
