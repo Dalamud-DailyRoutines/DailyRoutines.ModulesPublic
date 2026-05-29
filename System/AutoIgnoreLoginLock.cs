9885ec8b67090200eae990513592628175380cda
AtmoOmen <atmoomen2004@outlook.com>
2026-05-29 14:28:50 +0800

    AutoIgnoreLoginLock

--- System/AutoIgnoreLoginLock.cs
+++ System/AutoIgnoreLoginLock.cs
@@ -3,6 +3,7 @@ using DailyRoutines.Common.Module.Enums;
 using DailyRoutines.Common.Module.Models;
 using Dalamud.Hooking;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using OmenTools.Interop.Game;
 using OmenTools.Interop.Game.Lumina;
 using OmenTools.Interop.Game.Models;
 
@@ -18,16 +19,39 @@ public unsafe class AutoIgnoreLoginLock : ModuleBase
     };
 
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    private Hook<AgentLobby.Delegates.Update> AgentLobbyUpdateHook;
     
-    private static readonly CompSig AgentLobbyUpdateSig =
-        new("40 55 56 41 55 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 48 83 B9");
-    private delegate void                            AgentLobbyUpdateDelegate(AgentLobby* agent, uint deltaTime);
-    private          Hook<AgentLobbyUpdateDelegate>? AgentLobbyUpdateHook;
+    private delegate byte TimerDelegate(void* timer, int intervalSecond, int retryCount);
+
+    private static readonly CompSig             Timer0Sig = new("40 57 41 57 48 83 EC ?? 48 8B 05 ?? ?? ?? ?? 45 8B F8");
+    private                 Hook<TimerDelegate> Timer0Hook;
+    
+    private static readonly CompSig             Timer1Sig = new("40 53 57 48 83 EC ?? 48 8B F9 41 8B D8");
+    private                 Hook<TimerDelegate> Timer1Hook;
+
+    private readonly MemoryPatch loginFallbackPatch = new
+    (
+        "48 81 BE ?? ?? ?? ?? ?? ?? ?? ?? 76",
+        [
+            0x48, 0x81, 0xBE, 0x50, 0x12, 0x00, 0x00, // CMP [rsi+1250h], imm32
+            0xE8, 0x03, 0x00, 0x00,                   // imm32 = 0x3E7 (1000ms)
+            0x76, 0x16                                // JBE rel8
+        ]
+    );
 
     protected override void Init()
     {
-        AgentLobbyUpdateHook = AgentLobbyUpdateSig.GetHook<AgentLobbyUpdateDelegate>(AgentLobbyUpdateDetour);
+        AgentLobbyUpdateHook = AgentLobby.Instance()->VirtualTable->HookVFuncFromName("Update", (AgentLobby.Delegates.Update)AgentLobbyUpdateDetour);
         AgentLobbyUpdateHook.Enable();
+        
+        Timer0Hook = Timer0Sig.GetHook<TimerDelegate>(Timer0Detour);
+        Timer1Hook = Timer1Sig.GetHook<TimerDelegate>(Timer1Detour);
+        
+        Timer0Hook.Enable();
+        Timer1Hook.Enable();
+        
+        loginFallbackPatch.Enable();
     }
 
     private void AgentLobbyUpdateDetour(AgentLobby* agent, uint deltaTime)
@@ -36,4 +60,10 @@ public unsafe class AutoIgnoreLoginLock : ModuleBase
         AgentLobbyUpdateHook.Original(agent, deltaTime);
         agent->TemporaryLocked = false;
     }
+    
+    private byte Timer0Detour(void* timer, int intervalSecond, int retryCount) =>
+        Timer0Hook.Original(timer, 1, retryCount);
+
+    private byte Timer1Detour(void* timer, int intervalSecond, int retryCount) =>
+        Timer1Hook.Original(timer, 1, retryCount);
 }
