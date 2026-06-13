14410fd5a524e772d6df7fdc126e1403a947a49d
AtmoOmen <atmoomen2004@outlook.com>
2026-06-13 19:23:18 +0800

    TheCuffOfTheFatherHelper

--- Duty/TheCuffOfTheFatherHelper.cs
+++ Duty/TheCuffOfTheFatherHelper.cs
@@ -1,59 +1,108 @@
-using DailyRoutines.Common.Module.Abstractions;
 using DailyRoutines.Common.Module.Enums;
 using DailyRoutines.Common.Module.Models;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Game.ClientState.Conditions;
+using FFXIVClientStructs.FFXIV.Client.Game.Event;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using OmenTools.Interop.Game.Lumina;
 using OmenTools.OmenService;
+using OmenTools.OmenService.ImGuiZoneObject;
+using ModuleBase = DailyRoutines.Common.Module.Abstractions.ModuleBase;
 using ObjectKind = Dalamud.Game.ClientState.Objects.Enums.ObjectKind;
 
 namespace DailyRoutines.ModulesPublic.Duty;
 
-public class TheCuffOfTheFatherHelper : ModuleBase
+public unsafe class TheCuffOfTheFatherHelper : ModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title       = Lang.Get("TheCuffOfTheFatherHelperTitle"),
-        Description = Lang.Get("TheCuffOfTheFatherHelperDescription"),
-        Category    = ModuleCategory.Duty
+        Title = Lang.Get("TheCuffOfTheFatherHelperTitle"),
+        Description = Lang.Get
+        (
+            "TheCuffOfTheFatherHelperDescription",
+            LuminaWrapper.GetContentName(113), // 启动之章 2
+            LuminaWrapper.GetBNPCName(3751),   // 7 号哥布林战车
+            LuminaWrapper.GetBNPCName(2667)    // 炸弹
+        ),
+        Category = ModuleCategory.Duty
     };
 
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    private ZoneIndicatorHandle? handle;
+    
+    private List<IGameObject> bombObjects = [];
 
     protected override void Init()
     {
         DService.Instance().ClientState.TerritoryChanged += OnZoneChanged;
         OnZoneChanged(0);
+
+        handle = ZoneIndicatorRenderer.Instance().RegPermanent
+        (
+            443,
+            () => bombObjects,
+            gameObject => new()
+            {
+                Text      = $"→ {gameObject.Name} ←",
+                TextScale = 1.4f,
+                TextColor = ColorHelper.GetColor(518)
+            },
+            options: new()
+            {
+                Surrounding = new()
+                {
+                    Type      = ZoneIndicatorSurrounding.Shape.Circle,
+                    Radius    = 25,
+                    Thickness = 4f
+                }
+            }
+        );
     }
 
     protected override void Uninit()
     {
         DService.Instance().ClientState.TerritoryChanged -= OnZoneChanged;
         DService.Instance().AddonLifecycle.UnregisterListener(OnAddon);
+        
+        handle?.Unreg();
+        handle = null;
+        
+        bombObjects.Clear();
     }
 
-    private static void OnZoneChanged(uint u)
+    private void OnZoneChanged(uint u)
     {
         DService.Instance().AddonLifecycle.UnregisterListener(OnAddon);
+        bombObjects.Clear();
 
         if (GameState.TerritoryType != 443) return;
 
-        DService.Instance().AddonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, "_EnemyList", OnAddon);
+        DService.Instance().AddonLifecycle.RegisterListener(AddonEvent.PreRequestedUpdate, "_EnemyList", OnAddon);
     }
 
-    private static unsafe void OnAddon(AddonEvent type, AddonArgs args)
+    private void OnAddon(AddonEvent type, AddonArgs args)
     {
         var enemyListArray = AtkStage.Instance()->GetNumberArrayData(NumberArrayType.EnemyList);
         if (enemyListArray == null) return;
 
         var enemyCount = enemyListArray->IntArray[1];
         if (enemyCount < 1) return;
+        
+        var director = EventFramework.Instance()->GetContentDirector();
+        if (director == null) return;
 
+        // 还没到关底
+        var todos = director->DirectorTodos;
+        if (todos[2].CurrentCount != 1) return;
+
+        List<IGameObject> bombs = [];
         for (var i = 0; i < enemyCount; i++)
         {
-            var offset = 8 + i * 6;
+            var offset = 8 + (i * 6);
 
             var gameObjectID = (ulong)enemyListArray->IntArray[offset];
             if (gameObjectID is 0 or 0xE0000000) continue;
@@ -65,6 +114,8 @@ public class TheCuffOfTheFatherHelper : ModuleBase
                 }
                 obj)
                 continue;
+            
+            bombs.Add(obj);
 
             if (DService.Instance().Condition[ConditionFlag.Mounted])
                 obj.ToStruct()->TargetableStatus |= ObjectTargetableFlags.IsTargetable;
@@ -73,5 +124,7 @@ public class TheCuffOfTheFatherHelper : ModuleBase
 
             obj.ToStruct()->Highlight(ObjectHighlightColor.Yellow);
         }
+
+        bombObjects = bombs;
     }
 }
