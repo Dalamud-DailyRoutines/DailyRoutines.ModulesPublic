8ac05ddd473420bdc3b371d9c1490ff5158a2433
AtmoOmen <atmoomen2004@outlook.com>
2026-06-13 23:34:52 +0800

    Alphascape3Helper

--- Duty/Alphascape3Helper.cs
+++ Duty/Alphascape3Helper.cs
@@ -1,19 +1,28 @@
+using System.Numerics;
 using DailyRoutines.Common.Module.Abstractions;
 using DailyRoutines.Common.Module.Enums;
 using DailyRoutines.Common.Module.Models;
-using Dalamud.Game.ClientState.Objects.Enums;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using OmenTools.Interop.Game.Lumina;
 using OmenTools.OmenService;
 
 namespace DailyRoutines.ModulesPublic.Duty;
 
-public class Alphascape3Helper : ModuleBase
+public unsafe class Alphascape3Helper : ModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title       = Lang.Get("Alphascape3HelperTitle"),
-        Description = Lang.Get("Alphascape3HelperDescription"),
-        Category    = ModuleCategory.Duty
+        Title = Lang.Get("Alphascape3HelperTitle"),
+        Description = Lang.Get
+        (
+            "Alphascape3HelperDescription",
+            LuminaWrapper.GetContentName(589), // 欧米茄时空狭缝 阿尔法幻境3
+            LuminaWrapper.GetBNPCName(7852),   // 雷力投射点
+            LuminaWrapper.GetActionName(12911) // 欧米茄干扰器
+        ),
+        Category = ModuleCategory.Duty
     };
 
     protected override void Init()
@@ -25,30 +34,84 @@ public class Alphascape3Helper : ModuleBase
     protected override void Uninit()
     {
         DService.Instance().ClientState.TerritoryChanged -= OnZoneChanged;
+
         FrameworkManager.Instance().Unreg(OnUpdate);
+        UseActionManager.Instance().Unreg(OnStartCast);
+        UseActionManager.Instance().Unreg(OnCompleteCast);
     }
 
     private static void OnZoneChanged(uint u)
     {
         FrameworkManager.Instance().Unreg(OnUpdate);
-        if (GameState.TerritoryType != 800) return;
+        UseActionManager.Instance().Unreg(OnStartCast);
+        UseActionManager.Instance().Unreg(OnCompleteCast);
 
-        FrameworkManager.Instance().Reg(OnUpdate, 100);
+        if (GameState.TerritoryType != DUTY_ZONE_ID) return;
+
+        UseActionManager.Instance().RegPostCharacterStartCast(OnStartCast);
+        UseActionManager.Instance().RegPostCharacterCompleteCast(OnCompleteCast);
+    }
+
+    private static void OnCompleteCast
+    (
+        bool         result,
+        IBattleChara player,
+        ActionType   type,
+        uint         actionID,
+        uint         spellID,
+        GameObjectId animationTargetID,
+        Vector3      location,
+        float        rotation,
+        short        lastUsedActionSequence,
+        int          animationVariation,
+        int          ballistaEntityID
+    )
+    {
+        if (actionID != TRIANGLE_ATTACK_ACTION_ID) return;
+
+        FrameworkManager.Instance().Unreg(OnUpdate);
+    }
+
+    private static void OnStartCast
+    (
+        bool         result,
+        IBattleChara player,
+        ActionType   type,
+        uint         actionID,
+        nint         a4,
+        float        rotation,
+        float        a6
+    )
+    {
+        if (actionID != TRIANGLE_ATTACK_ACTION_ID) return;
+
+        FrameworkManager.Instance().Unreg(OnUpdate);
+        FrameworkManager.Instance().Reg(OnUpdate, 500);
     }
 
     private static void OnUpdate(IFramework framework)
     {
-        if (GameState.TerritoryType != 800)
-        {
-            FrameworkManager.Instance().Unreg(OnUpdate);
-            return;
-        }
+        var chara = CharacterManager.Instance()->FindFirst(&FindPoint);
+        if (chara == null) return;
 
-        if (DService.Instance().ObjectTable.LocalPlayer is null) return;
+        UseActionManager.Instance().UseAction(ActionType.Action, QUEST_ACTION_ID, chara->EntityId);
+        return;
 
-        var obj = DService.Instance().ObjectTable.FirstOrDefault(x => x is { ObjectKind: ObjectKind.BattleNpc, DataID: 9638 });
-        if (obj is not { IsTargetable: true }) return;
-
-        UseActionManager.Instance().UseAction(ActionType.Action, 12911, obj.EntityID);
+        static bool FindPoint(BattleChara* chara) =>
+            chara             != null                 &&
+            chara->ObjectKind == ObjectKind.BattleNpc &&
+            chara->BaseId     == POINT_DATA_ID;
     }
+
+    #region 常量
+
+    private const uint DUTY_ZONE_ID = 800;
+
+    private const uint TRIANGLE_ATTACK_ACTION_ID = 12923;
+
+    private const uint POINT_DATA_ID = 9638;
+
+    private const uint QUEST_ACTION_ID = 12911;
+
+    #endregion
 }
