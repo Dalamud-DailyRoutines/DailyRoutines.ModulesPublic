f40d87dd8b17dbd0a13a99799d4a02f2f4511b09
AtmoOmen <atmoomen2004@outlook.com>
2026-09-14 20:17:35 +0800

    AutoBlockAgelessWordsAndSolidReason

---
+++ CraftGather/AutoBlockAgelessWordsAndSolidReason.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+using DailyRoutines.Common.Module.Abstractions;
+using DailyRoutines.Common.Module.Enums;
+using DailyRoutines.Common.Module.Models;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using OmenTools.Interop.Game.Lumina;
+using OmenTools.OmenService;
+
+namespace DailyRoutines.ModulesPublic.CraftGather;
+
+public class AutoBlockAgelessWordsAndSolidReason : ModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = Lang.Get("AutoBlockAgelessWordsAndSolidReasonTitle"),
+        Description = Lang.Get("AutoBlockAgelessWordsAndSolidReasonDescription"),
+        Category    = ModuleCategory.CraftGather
+    };
+
+    public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    protected override void Init() =>
+        UseActionManager.Instance().RegPreUseActionLocation(OnUseAction);
+
+    protected override void Uninit() =>
+        UseActionManager.Instance().Unreg(OnUseAction);
+
+    private static void OnUseAction
+    (
+        ref bool       isPrevented,
+        ref ActionType type,
+        ref uint       actionID,
+        ref ulong      targetID,
+        ref Vector3    location,
+        ref uint       extraParam,
+        ref byte       a7
+    )
+    {
+        if (type != ActionType.Action) 
+            return;
+        
+        if (actionID != SOLID_REASON_ACTION_ID && actionID != AGELESS_WORDS_ACTION_ID)
+            return;
+        
+        if (!LocalPlayerState.HasStatus(EUREKA_MOMENT_STATUS_ID, out _))
+            return;
+
+        isPrevented = true;
+
+        var message = Lang.Get
+        (
+            "AutoBlockAgelessWordsAndSolidReason-Notification-Blocked",
+            LuminaWrapper.GetStatusName(EUREKA_MOMENT_STATUS_ID),
+            LuminaWrapper.GetActionName(actionID)
+        );
+        NotifyHelper.ToastError(message);
+    }
+
+    #region 常量
+
+    // 石工之理
+    private const uint SOLID_REASON_ACTION_ID  = 232;
+    
+    // 农夫之智
+    private const uint AGELESS_WORDS_ACTION_ID = 215;
+
+    // 理智同兴预备
+    private const uint EUREKA_MOMENT_STATUS_ID = 2765;
+
+    #endregion
+}
