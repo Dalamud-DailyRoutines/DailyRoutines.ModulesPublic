2eaef1714231ee7060f83dd06364d69334d275fe
AtmoOmen <atmoomen2004@outlook.com>
2026-09-19 21:51:44 +0800

    AutoAdjustNamePlateIcon

--- Combat/AutoAdjustNamePlateIcon.cs
+++ Combat/AutoAdjustNamePlateIcon.cs
@@ -6,6 +6,8 @@ using DailyRoutines.Extensions;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using OmenTools.Interop.Game.Models;
 
 namespace DailyRoutines.ModulesPublic;
 
@@ -21,17 +23,37 @@ public unsafe class AutoAdjustNamePlateIcon : ModuleBase
 
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
 
+    private static readonly CompSig AtkUldManagerUpdateFromParentNodeSig = new("48 89 5C 24 ?? 55 56 57 48 83 EC ?? 41 0F B6 F1");
+    private delegate void AtkUldManagerUpdateFromParentNodeDelegate
+    (
+        AtkUldManager* manager,
+        AtkResNode*    node,
+        AtkResNode*    parentNode,
+        byte           onlyDirty
+    );
+    private AtkUldManagerUpdateFromParentNodeDelegate AtkUldManagerUpdateFromParentNode = null!;
+
+    private readonly Vector2[] basePositions    = new Vector2[AddonNamePlate.NumNamePlateObjects];
+    private readonly Vector2[] appliedPositions = new Vector2[AddonNamePlate.NumNamePlateObjects];
+    private readonly bool[]    applied          = new bool[AddonNamePlate.NumNamePlateObjects];
+    
     private Config config = null!;
 
     protected override void Init()
     {
         config = Config.Load(this) ?? new();
 
-        IAddonLifecycle.Instance().RegisterListener(AddonEvent.PreRequestedUpdate, "NamePlate", OnAddon);
+        AtkUldManagerUpdateFromParentNode = AtkUldManagerUpdateFromParentNodeSig.GetDelegate<AtkUldManagerUpdateFromParentNodeDelegate>();
+
+        IAddonLifecycle.Instance().RegisterListener(AddonEvent.PostRequestedUpdate, "NamePlate", OnAddonEvent);
+        IAddonLifecycle.Instance().RegisterListener(AddonEvent.PostUpdate,          "NamePlate", OnAddonEvent);
     }
 
-    protected override void Uninit() =>
-        IAddonLifecycle.Instance().UnregisterListener(OnAddon);
+    protected override void Uninit()
+    {
+        IAddonLifecycle.Instance().UnregisterListener(OnAddonEvent);
+        ResetIcons();
+    }
 
     protected override void ConfigUI()
     {
@@ -42,32 +64,87 @@ public unsafe class AutoAdjustNamePlateIcon : ModuleBase
             config.Save(this);
     }
 
-    private void OnAddon
+    private void OnAddonEvent
     (
         AddonEvent type,
         AddonArgs? args
     )
     {
-        var addon = (AddonNamePlate*)NamePlate;
-        if (!NamePlate->IsAddonAndNodesReady()) return;
+        var unitBase = NamePlate;
+        if (unitBase == null || !unitBase->IsAddonAndNodesReady()) return;
 
-        for (var i = 0; i < 50; i++)
+        var addon = (AddonNamePlate*)unitBase;
+        if (addon->NamePlateObjectArray == null || addon->RootNode == null) return;
+
+        for (var i = 0; i < AddonNamePlate.NumNamePlateObjects; i++)
         {
-            var obj = addon->NamePlateObjectArray[i];
-            if (!obj.IsVisible || !obj.MarkerIcon->IsVisible())
-                continue;
+            var icon = addon->NamePlateObjectArray[i].MarkerIcon;
+            if (icon == null || !icon->IsVisible()) continue;
 
-            var imageNode = obj.MarkerIcon;
-            if (imageNode == null) return;
-
-            var scale = config.Scale;
-            var iconW = imageNode->Width;
-            var iconH = imageNode->Height;
-            var compX = iconW * (1f - scale) / 2f;
-            var compY = iconH * (1f - scale) / 2f;
-            imageNode->SetScale(scale, scale);
-            imageNode->SetPositionFloat(96f + config.Offset.X + compX, 4f + config.Offset.Y + compY);
+            ApplyIcon(icon, i, config.Scale, config.Offset);
         }
+
+        RefreshNodeTransforms(addon);
+    }
+
+    private void ApplyIcon
+    (
+        AtkImageNode* icon,
+        int           index,
+        float         scale,
+        Vector2       offset
+    )
+    {
+        var current = new Vector2(icon->X, icon->Y);
+        if (current != appliedPositions[index])
+            basePositions[index] = current;
+
+        var target = basePositions[index] + offset;
+
+        icon->SetOrigin(icon->Width / 2f, icon->Height / 2f);
+        icon->SetScale(scale, scale);
+        icon->SetPositionFloat(target.X, target.Y);
+
+        appliedPositions[index] = target;
+        applied[index]          = true;
+    }
+
+    private void RefreshNodeTransforms
+    (
+        AddonNamePlate* addon
+    )
+    {
+        addon->RootNode->DrawFlags |= 1;
+        AtkUldManagerUpdateFromParentNode(&addon->UldManager, addon->RootNode, addon->RootNode->ParentNode, 1);
+    }
+
+    private void ResetIcons()
+    {
+        var unitBase = NamePlate;
+        if (unitBase == null) return;
+
+        var addon = (AddonNamePlate*)unitBase;
+        if (addon->NamePlateObjectArray == null || addon->RootNode == null) return;
+
+        var modified = false;
+
+        for (var i = 0; i < AddonNamePlate.NumNamePlateObjects; i++)
+        {
+            if (!applied[i]) continue;
+
+            var icon = addon->NamePlateObjectArray[i].MarkerIcon;
+            if (icon == null) continue;
+
+            icon->SetOrigin(0f, 0f);
+            icon->SetScale(1f, 1f);
+            icon->SetPositionFloat(basePositions[i].X, basePositions[i].Y);
+
+            applied[i] = false;
+            modified   = true;
+        }
+
+        if (modified)
+            RefreshNodeTransforms(addon);
     }
 
     private class Config : ModuleConfig
