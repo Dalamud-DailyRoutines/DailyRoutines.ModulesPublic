08185e630aba15f0453255c02025337b50da355a
AtmoOmen <atmoomen2004@outlook.com>
2026-09-19 15:23:10 +0800

    自动勾选简易采集

---
+++ CraftGather/AutoCheckQuickGathering.cs
@@ -0,0 +1,37 @@
+﻿using DailyRoutines.Common.Module.Abstractions;
+using DailyRoutines.Common.Module.Enums;
+using DailyRoutines.Common.Module.Models;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+
+namespace DailyRoutines.ModulesPublic.CraftGather;
+
+public unsafe class AutoCheckQuickGathering : ModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = Lang.Get("AutoCheckQuickGatheringTitle"),
+        Description = Lang.Get("AutoCheckQuickGatheringDescription"),
+        Category    = ModuleCategory.CraftGather
+    };
+
+    public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    protected override void Init() =>
+        IAddonLifecycle.Instance().RegisterListener(AddonEvent.PostSetup, "Gathering", OnAddon);
+    
+    protected override void Uninit() =>
+        IAddonLifecycle.Instance().UnregisterListener(OnAddon);
+
+    private static void OnAddon
+    (
+        AddonEvent type,
+        AddonArgs  args
+    )
+    {
+        var addon = args.Addon.ToStruct();
+        if (addon == null) return;
+        
+        addon->Callback(130, true);
+    }
+}
